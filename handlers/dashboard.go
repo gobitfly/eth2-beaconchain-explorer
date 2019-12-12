dca55eb1299180956ac3fdbe36484b335f522b93
Patrick Pfeiffer <patrick@bitfly.at>
2019-12-12 15:29:34 +0100

    Dashboard: make chart-data available via endpoints

--- handlers/dashboard.go
+++ handlers/dashboard.go
@@ -20,6 +20,10 @@ var dashboardTemplate = template.Must(template.New("dashboard").ParseFiles("temp
 var dashboardNotFoundTemplate = template.Must(template.New("dashboardnotfound").ParseFiles("templates/layout.html", "templates/dashboardnotfound.html"))
 
 func parseValidatorsFromQueryString(str string) ([]uint64, error) {
+	if str == "" {
+		return []uint64{}, nil
+	}
+
 	strSplit := strings.Split(str, ",")
 	strSplitLen := len(strSplit)
 
@@ -189,7 +193,138 @@ func Dashboard(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func DashboardValidatorsDataPending(w http.ResponseWriter, r *http.Request) {
+func DashboardDataBalance(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	q := r.URL.Query()
+
+	filterArr, err := parseValidatorsFromQueryString(q.Get("validators"))
+	if err != nil {
+		logger.WithError(err).Error("Failed parsing validators from query string")
+		http.Error(w, err.Error(), 404)
+		return
+	}
+	filter := pq.Array(filterArr)
+
+	var balanceHistory []*types.ValidatorBalanceHistory
+	err = db.DB.Select(&balanceHistory, "SELECT epoch, SUM(balance) as balance FROM validator_balances WHERE validatorindex = ANY($1) GROUP BY epoch ORDER BY epoch", filter)
+	if err != nil {
+		logger.Printf("Error retrieving validator balance history: %v", err)
+		http.Error(w, "Internal server error", 503)
+		return
+	}
+
+	balanceHistoryChartData := make([][]float64, len(balanceHistory))
+	for i, balance := range balanceHistory {
+		balanceHistoryChartData[i] = []float64{float64(utils.EpochToTime(balance.Epoch).Unix() * 1000), float64(balance.Balance) / 1000000000}
+	}
+
+	var effectiveBalanceHistory []*types.DashboardValidatorBalanceHistory
+	err = db.DB.Select(&effectiveBalanceHistory, "SELECT epoch, SUM(effectivebalance) as balance, COUNT(*) as validatorcount FROM validator_set WHERE validatorindex = ANY($1) GROUP BY epoch ORDER BY epoch", filter)
+	if err != nil {
+		logger.Printf("Error retrieving validator effective balance history: %v", err)
+		http.Error(w, "Internal server error", 503)
+		return
+	}
+
+	effectiveBalanceHistoryChartData := make([][]float64, len(effectiveBalanceHistory))
+	for i, balance := range effectiveBalanceHistory {
+		effectiveBalanceHistoryChartData[i] = []float64{float64(utils.EpochToTime(balance.Epoch).Unix() * 1000), float64(balance.Balance) / 1000000000, balance.ValidatorCount}
+	}
+
+	type dataType struct {
+		BalanceHistory          [][]float64 `json:"balanceHistory"`
+		EffectiveBalanceHistory [][]float64 `json:"effectiveBalanceHistory"`
+	}
+	data := &dataType{
+		BalanceHistory:          balanceHistoryChartData,
+		EffectiveBalanceHistory: effectiveBalanceHistoryChartData,
+	}
+
+	err = json.NewEncoder(w).Encode(data)
+	if err != nil {
+		logger.Fatalf("Error enconding json response for %v route: %v", r.URL.String(), err)
+	}
+}
+
+func DashboardDataProposals(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	q := r.URL.Query()
+
+	filterArr, err := parseValidatorsFromQueryString(q.Get("validators"))
+	if err != nil {
+		logger.WithError(err).Error("Failed parsing validators from query string")
+		http.Error(w, err.Error(), 404)
+		return
+	}
+	filter := pq.Array(filterArr)
+
+	proposals := []struct {
+		Day    uint64
+		Status uint64
+		Count  uint
+	}{}
+
+	err = db.DB.Select(&proposals, "select slot / 7200 as day, status, count(*) FROM blocks WHERE proposer = ANY($1) group by day, status order by day;", filter)
+	if err != nil {
+		logger.WithError(err).Error("Error retrieving Daily Proposed Blocks blocks count")
+		http.Error(w, "Internal server error", 503)
+		return
+	}
+
+	dailyProposalCount := []types.DailyProposalCount{}
+
+	for i := 0; i < len(proposals); i++ {
+		if i == len(proposals)-1 {
+			if proposals[i].Status == 1 {
+				dailyProposalCount = append(dailyProposalCount, types.DailyProposalCount{
+					Day:      utils.SlotToTime(proposals[i].Day * 7200).Unix(),
+					Proposed: proposals[i].Count,
+					Missed:   0,
+				})
+			} else if proposals[i].Status == 2 {
+				dailyProposalCount = append(dailyProposalCount, types.DailyProposalCount{
+					Day:      utils.SlotToTime(proposals[i].Day * 7200).Unix(),
+					Proposed: 0,
+					Missed:   proposals[i].Count,
+				})
+			} else {
+				logger.WithError(err).Error("Error parsing Daily Proposed Blocks unkown status")
+			}
+		} else {
+			if proposals[i].Day == proposals[i+1].Day {
+				dailyProposalCount = append(dailyProposalCount, types.DailyProposalCount{
+					Day:      utils.SlotToTime(proposals[i].Day * 7200).Unix(),
+					Proposed: proposals[i].Count,
+					Missed:   proposals[i+1].Count,
+				})
+				i++
+			} else if proposals[i].Status == 1 {
+				dailyProposalCount = append(dailyProposalCount, types.DailyProposalCount{
+					Day:      utils.SlotToTime(proposals[i].Day * 7200).Unix(),
+					Proposed: proposals[i].Count,
+					Missed:   0,
+				})
+			} else if proposals[i].Status == 2 {
+				dailyProposalCount = append(dailyProposalCount, types.DailyProposalCount{
+					Day:      utils.SlotToTime(proposals[i].Day * 7200).Unix(),
+					Proposed: 0,
+					Missed:   proposals[i].Count,
+				})
+			} else {
+				logger.WithError(err).Error("Error parsing Daily Proposed Blocks unkown status")
+			}
+		}
+	}
+
+	err = json.NewEncoder(w).Encode(dailyProposalCount)
+	if err != nil {
+		logger.Fatalf("Error enconding json response for %v route: %v", r.URL.String(), err)
+	}
+}
+
+func DashboardDataValidatorsPending(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	q := r.URL.Query()
@@ -236,26 +371,27 @@ func DashboardValidatorsDataPending(w http.ResponseWriter, r *http.Request) {
 
 	var validators []*types.ValidatorsPageDataValidators
 	err = db.DB.Select(&validators, `SELECT 
-            validator_set.epoch,
-            validator_set.validatorindex, 
-            validators.pubkey, 
-            validator_set.withdrawableepoch, 
-            validator_set.effectivebalance, 
-            validator_set.slashed, 
-            validator_set.activationeligibilityepoch, 
-            validator_set.activationepoch, 
-            validator_set.exitepoch,
-            validator_balances.balance
-        FROM validator_set
-        LEFT JOIN validator_balances 
-            ON validator_set.epoch = validator_balances.epoch
-            AND validator_set.validatorindex = validator_balances.validatorindex
-        LEFT JOIN validators ON validator_set.validatorindex = validators.validatorindex
-        WHERE validator_set.epoch = $1 
-            AND validator_set.epoch < activationepoch
-            AND validator_set.validatorindex = ANY($5)
-        ORDER BY activationepoch DESC 
-        LIMIT $2 OFFSET $3`, services.LatestEpoch(), length, start, filter)
+			validator_set.epoch,
+			validator_set.validatorindex, 
+			validators.pubkey, 
+			validator_set.withdrawableepoch, 
+			validator_set.effectivebalance, 
+			validator_set.slashed, 
+			validator_set.activationeligibilityepoch, 
+			validator_set.activationepoch, 
+			validator_set.exitepoch,
+			validator_balances.balance
+		FROM validator_set
+		LEFT JOIN validator_balances 
+			ON validator_set.epoch = validator_balances.epoch
+			AND validator_set.validatorindex = validator_balances.validatorindex
+		LEFT JOIN validators 
+			ON validator_set.validatorindex = validators.validatorindex
+		WHERE validator_set.epoch = $1 
+			AND validator_set.epoch < activationepoch
+			AND validator_set.validatorindex = ANY($5)
+		ORDER BY activationepoch DESC 
+		LIMIT $2 OFFSET $3`, services.LatestEpoch(), length, start, filter)
 
 	if err != nil {
 		logger.Printf("Error retrieving pending validator data: %v", err)
@@ -289,7 +425,7 @@ func DashboardValidatorsDataPending(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func DashboardValidatorsDataActive(w http.ResponseWriter, r *http.Request) {
+func DashboardDataValidatorsActive(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	q := r.URL.Query()
@@ -341,29 +477,29 @@ func DashboardValidatorsDataActive(w http.ResponseWriter, r *http.Request) {
 
 	var validators []*types.ValidatorsPageDataValidators
 	err = db.DB.Select(&validators, `SELECT 
-            validator_set.epoch, 
-            validator_set.validatorindex, 
-            validators.pubkey, 
-            validator_set.withdrawableepoch, 
-            validator_set.effectivebalance, 
-            validator_set.slashed, 
-            validator_set.activationeligibilityepoch, 
-            validator_set.activationepoch, 
-            validator_set.exitepoch,
-            validator_balances.balance
-        FROM validator_set
-        LEFT JOIN validator_balances 
-            ON validator_set.epoch = validator_balances.epoch
-            AND validator_set.validatorindex = validator_balances.validatorindex
-        LEFT JOIN validators
-            ON validator_set.validatorindex = validators.validatorindex
-        WHERE validator_set.epoch = $1 
-            AND validator_set.epoch > activationepoch 
-            AND validator_set.epoch < exitepoch 
-            AND encode(validators.pubkey::bytea, 'hex') LIKE $2
-            AND validator_set.validatorindex = ANY($5)
-        ORDER BY activationepoch DESC 
-        LIMIT $3 OFFSET $4`, services.LatestEpoch(), "%"+search+"%", length, start, filter)
+			validator_set.epoch, 
+			validator_set.validatorindex, 
+			validators.pubkey, 
+			validator_set.withdrawableepoch, 
+			validator_set.effectivebalance, 
+			validator_set.slashed, 
+			validator_set.activationeligibilityepoch, 
+			validator_set.activationepoch, 
+			validator_set.exitepoch,
+			validator_balances.balance
+		FROM validator_set
+		LEFT JOIN validator_balances 
+			ON validator_set.epoch = validator_balances.epoch
+			AND validator_set.validatorindex = validator_balances.validatorindex
+		LEFT JOIN validators 
+			ON validator_set.validatorindex = validators.validatorindex
+		WHERE validator_set.epoch = $1 
+			AND validator_set.epoch > activationepoch 
+			AND validator_set.epoch < exitepoch 
+			AND encode(validators.pubkey::bytea, 'hex') LIKE $2
+			AND validator_set.validatorindex = ANY($5)
+		ORDER BY activationepoch DESC 
+		LIMIT $3 OFFSET $4`, services.LatestEpoch(), "%"+search+"%", length, start, filter)
 
 	if err != nil {
 		logger.Printf("Error retrieving active validators data: %v", err)
@@ -397,7 +533,7 @@ func DashboardValidatorsDataActive(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func DashboardValidatorsDataEjected(w http.ResponseWriter, r *http.Request) {
+func DashboardDataValidatorsEjected(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	q := r.URL.Query()
@@ -449,26 +585,28 @@ func DashboardValidatorsDataEjected(w http.ResponseWriter, r *http.Request) {
 
 	var validators []*types.ValidatorsPageDataValidators
 	err = db.DB.Select(&validators, `SELECT 
-            validator_set.epoch,
-            validator_set.validatorindex, 
-            validators.pubkey, 
-            validator_set.withdrawableepoch, 
-            validator_set.effectivebalance, 
-            validator_set.slashed, 
-            validator_set.activationeligibilityepoch, 
-            validator_set.activationepoch, 
-            validator_set.exitepoch,
-            validator_balances.balance
-        FROM validator_set 
-        LEFT JOIN validator_balances ON validator_set.epoch = validator_balances.epoch
-            AND validator_set.validatorindex = validator_balances.validatorindex
-        LEFT JOIN validators ON validator_set.validatorindex = validators.validatorindex
-        WHERE validator_set.epoch = $1 
-            AND validator_set.epoch > exitepoch
-            AND encode(validators.pubkey::bytea, 'hex') LIKE $2
-            AND validator_set.validatorindex = ANY($5)
-        ORDER BY activationepoch DESC 
-        LIMIT $3 OFFSET $4`, services.LatestEpoch(), "%"+search+"%", length, start, filter)
+			validator_set.epoch,
+			validator_set.validatorindex, 
+			validators.pubkey, 
+			validator_set.withdrawableepoch, 
+			validator_set.effectivebalance, 
+			validator_set.slashed, 
+			validator_set.activationeligibilityepoch, 
+			validator_set.activationepoch, 
+			validator_set.exitepoch,
+			validator_balances.balance
+		FROM validator_set 
+		LEFT JOIN validator_balances 
+			ON validator_set.epoch = validator_balances.epoch
+			AND validator_set.validatorindex = validator_balances.validatorindex
+		LEFT JOIN validators 
+			ON validator_set.validatorindex = validators.validatorindex
+		WHERE validator_set.epoch = $1 
+			AND validator_set.epoch > exitepoch
+			AND encode(validators.pubkey::bytea, 'hex') LIKE $2
+			AND validator_set.validatorindex = ANY($5)
+		ORDER BY activationepoch DESC 
+		LIMIT $3 OFFSET $4`, services.LatestEpoch(), "%"+search+"%", length, start, filter)
 
 	if err != nil {
 		logger.Printf("Error retrieving ejected validators data: %v", err)
