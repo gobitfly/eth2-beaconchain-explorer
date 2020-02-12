01c2099bd063ace75c4d0e410dbc4ff8168b60dc
Peter Pratscher <ppratscher@users.noreply.github.com>
2020-02-12 14:00:19 +0100

    Preserver validator rank when filtering

--- handlers/validators_leaderboard.go
+++ handlers/validators_leaderboard.go
@@ -102,16 +102,16 @@ func ValidatorsLeaderboardData(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var performanceData []*types.ValidatorPerformance
-	err = db.DB.Select(&performanceData, `SELECT 
-											   ROW_NUMBER() OVER (ORDER BY `+orderBy+` DESC) AS rank,
-											   validator_performance.*,
-											   validators.pubkey 
-										FROM validator_performance 
-											LEFT JOIN validators ON validators.validatorindex = validator_performance.validatorindex
-										WHERE (encode(validators.pubkey::bytea, 'hex') LIKE $3
-													OR CAST(validator_performance.validatorindex AS text) LIKE $3)
-										ORDER BY `+orderBy+` `+orderDir+` 										
-										LIMIT $1 OFFSET $2`, length, start, "%"+search+"%")
+	err = db.DB.Select(&performanceData, `SELECT * FROM (SELECT 
+											   			ROW_NUMBER() OVER (ORDER BY `+orderBy+` DESC) AS rank,
+											   			validator_performance.*,
+											   			validators.pubkey 
+													FROM validator_performance 
+													LEFT JOIN validators ON validators.validatorindex = validator_performance.validatorindex
+													ORDER BY `+orderBy+` `+orderDir+`) AS a
+													WHERE (encode(a.pubkey::bytea, 'hex') LIKE $3
+														OR CAST(a.validatorindex AS text) LIKE $3)
+													LIMIT $1 OFFSET $2`, length, start, "%"+search+"%")
 
 	if err != nil {
 		logger.Errorf("error retrieving validator attestations data: %v", err)
