7e0f1a8362f94ed63a9584238ba045c59a8140fe
Patrick Pfeiffer <patrick@bitfly.at>
2020-03-09 11:52:06 +0100

    fix epoch-export

--- db/db.go
+++ db/db.go
@@ -250,13 +250,13 @@ func SaveEpoch(data *types.EpochData) error {
 			voluntaryexitscount, 
 			validatorscount, 
 			averagevalidatorbalance, 
-			totalvalidatorbalance
+			totalvalidatorbalance,
 			finalized, 
 			eligibleether, 
 			globalparticipationrate, 
 			votedether
 		)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) 
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) 
 		ON CONFLICT (epoch) DO UPDATE SET 
 			blockscount             = excluded.blockscount, 
 			proposerslashingscount  = excluded.proposerslashingscount,
