7b7ea7ab62faadbea58942d898f33417eb8e7cc0
Patrick Pfeiffer <patrick@bitfly.at>
2020-06-10 11:55:38 +0200

    fix merge

--- utils/utils.go
+++ utils/utils.go
@@ -25,18 +25,6 @@ const PageSize = 500
 // Config is the globally accessible configuration
 var Config *types.Config
 
-// FormatGraffitiString formats (and escapes) the graffiti
-func FormatGraffitiString(graffiti string) string {
-	return strings.Map(fixUtf, template.HTMLEscapeString(graffiti))
-}
-
-func fixUtf(r rune) rune {
-	if r == utf8.RuneError {
-		return -1
-	}
-	return r
-}
-
 // GetTemplateFuncs will get the template functions
 func GetTemplateFuncs() template.FuncMap {
 	return template.FuncMap{
@@ -69,6 +57,7 @@ func GetTemplateFuncs() template.FuncMap {
 	}
 }
 
+// FormatGraffitiString formats (and escapes) the graffiti
 func FormatGraffitiString(graffiti string) string {
 	return strings.Map(fixUtf, template.HTMLEscapeString(graffiti))
 }
