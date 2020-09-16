2696fcc38440dfad9de6f1a2a1b6d495aebe128d
Patrick Pfeiffer <patrick@bitfly.at>
2020-09-16 13:06:44 +0200

    wip

---
+++ rpc/eth2_api_v1.go
@@ -0,0 +1,14 @@
+package rpc
+
+// import "eth2-exporter/types"
+//
+// type Eth2ApiV1Client struct{}
+//
+// func (c *Eth2ApiV1Client) GetChainHead() (*types.ChainHead, error)                           {}
+// func (c *Eth2ApiV1Client) GetEpochData(epoch uint64) (*types.EpochData, error)               {}
+// func (c *Eth2ApiV1Client) GetValidatorQueue() (*types.ValidatorQueue, error)                 {}
+// func (c *Eth2ApiV1Client) GetAttestationPool() ([]*types.Attestation, error)                 {}
+// func (c *Eth2ApiV1Client) GetEpochAssignments(epoch uint64) (*types.EpochAssignments, error) {}
+// func (c *Eth2ApiV1Client) GetBlocksBySlot(slot uint64) ([]*types.Block, error)               {}
+// func (c *Eth2ApiV1Client) GetValidatorParticipation(epoch uint64) (*types.ValidatorParticipation, error) {
+// }
