01bab7b1ae92ab265b9784c7743cc8e5eeadb149
Stefan <22667037+qu0b@users.noreply.github.com>
2020-10-06 15:49:57 +0200

    Add requestInterval for requestAnimationFrame (#284)

---
+++ static/js/requestInterval.js
@@ -0,0 +1,45 @@
+window.requestAnimFrame = (function() {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function(/* function */ callback, /* DOMElement */ element) {
+      window.setTimeout(callback, 1000 / 60);
+    }
+  );
+})();
+
+/**
+ * Behaves the same as setInterval except uses requestAnimationFrame() where possible for better performance
+ * @param {function} fn The callback function
+ * @param {int} delay The delay in milliseconds
+ */
+window.requestInterval = function(fn, delay) {
+  if (
+    !window.requestAnimationFrame &&
+    !window.webkitRequestAnimationFrame &&
+    !(window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame) && // Firefox 5 ships without cancel support
+    !window.oRequestAnimationFrame &&
+    !window.msRequestAnimationFrame
+  )
+    return window.setInterval(fn, delay);
+
+  var start = new Date().getTime(),
+    handle = new Object();
+
+  function loop() {
+    var current = new Date().getTime(),
+      delta = current - start;
+
+    if (delta >= delay) {
+      fn.call();
+      start = new Date().getTime();
+    }
+    handle.value = requestAnimFrame(loop);
+  }
+
+  handle.value = requestAnimFrame(loop);
+  return handle;
+};
