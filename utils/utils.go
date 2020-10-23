94e8f23e3c3d78d3a67c2f4cf6e7de6a0fd9a61f
Butta <inan1deniz@gmail.com>
2020-10-23 09:28:23 +0200

    yolo

--- utils/utils.go
+++ utils/utils.go
@@ -177,6 +177,20 @@ func MustParseHex(hexString string) []byte {
 	return data
 }
 
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Headers:", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+		return
+	})
+}
+
 func IsApiRequest(r *http.Request) bool {
 	query, ok := r.URL.Query()["format"]
 	return ok && len(query) > 0 && query[0] == "json"
