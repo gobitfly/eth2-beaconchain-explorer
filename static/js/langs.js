aaa9a2fb4e1943191ca5f1291b6e15bfcccf8afa
Vahe <vahegian@gmail.com>
2020-11-17 22:32:28 +0100

    add language switching functionality

---
+++ static/js/langs.js
@@ -0,0 +1,24 @@
+const langs={
+    available:[{lang:"eng", icon:""}, {lang:"rus", icon:""}],
+    eng:{
+        preGenesis:{
+            p1_title: "Phase 0 Beacon Chain",
+            p1_text_1:"The Ethereum Beacon Chain is currently undergoing genesis. Please prepare your validators and send your deposits. To join Ethereum 2.0 fire up one of the beacon chain and validator clients. The Mainnet will support",
+            p1_text_2:"Please visit the official",
+            p1_text_3:"for more information on how to deposit.",
+        }
+    },
+
+    rus:{
+        preGenesis:{
+            p1_title: "Beacon Chain Фаза 0",
+            p1_text_1:"Настоящее время Ethereum Beacon Chain находится в стадии genesis. Пожалуйста, подготовьте ваши валидаторы и отправьте свои депозиты. Чтобы присоединиться к Ethereum 2.0, запустите одну из beacon chain валидаторов. Основная сеть будет поддерживать",
+            p1_text_2:"Пожалуйста зайдите в официальный веб сайт",
+            p1_text_3:"для подробней информации о депозитов.",
+        }
+    },
+
+    de:{
+
+    }
+}
