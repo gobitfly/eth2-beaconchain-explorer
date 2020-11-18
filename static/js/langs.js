b82a05ca119cdad0bc21d5e1b52f3497ba108fa5
Vahe <vahegian@gmail.com>
2020-11-18 11:47:49 +0100

    add translations for preGenesis.html

--- static/js/langs.js
+++ static/js/langs.js
@@ -1,11 +1,29 @@
 const langs={
-    available:[{lang:"eng", icon:""}, {lang:"rus", icon:""}],
+    available:[{lang:"eng", icon:null}, {lang:"rus", icon:null}],
     eng:{
         preGenesis:{
             p1_title: "Phase 0 Beacon Chain",
             p1_text_1:"The Ethereum Beacon Chain is currently undergoing genesis. Please prepare your validators and send your deposits. To join Ethereum 2.0 fire up one of the beacon chain and validator clients. The Mainnet will support",
             p1_text_2:"Please visit the official",
             p1_text_3:"for more information on how to deposit.",
+            p2_title:"When is genesis reached?",
+            p2_text_1:"Genesis will occur once enough deposits have been made to the deposit contract",
+            p2_text_2:"Each deposit is registered by the beacon chain after a block delay of",
+            p2_text_3:"Ethereum 1.0 blocks. The earliest possible genesis time and the estimated genesis time (according to the rate of deposits) can be seen on the right.",
+            p3_title:"Explore & Manage",
+            p3_text_1:"Once the beacon chain has reached genesis you can explore",
+            p3_text_2:"epochs",
+            p3_text_3:"slots",
+            p3_text_4:"and see",
+            p3_text_5:"statistics",
+            p3_text_6:"Further, you can monitor your validators performance using our",
+            p3_text_7:"dashboard",
+            p3_text_8:"feature",
+            p3_text_9:"Sign up",
+            p3_text_10:"and add alerts on the",
+            p3_text_11:"accounts page",
+            p3_text_12:"Get notified if your validators go offline. For more information about the beacon chain view our",
+            p3_text_13:"knowledge base."
         }
     },
 
@@ -15,6 +33,24 @@ const langs={
             p1_text_1:"Настоящее время Ethereum Beacon Chain находится в стадии genesis. Пожалуйста, подготовьте ваши валидаторы и отправьте свои депозиты. Чтобы присоединиться к Ethereum 2.0, запустите одну из beacon chain валидаторов. Основная сеть будет поддерживать",
             p1_text_2:"Пожалуйста зайдите в официальный веб сайт",
             p1_text_3:"для подробней информации о депозитов.",
+            p2_title: "Когда достигнет genesis?",
+            p2_text_1:"Genesis достигнет когда необходимое количество депозитов будут сделаны на депозит контракт",
+            p2_text_2:"Каждый депозит будет зарегистрировано с beacon chain после",
+            p2_text_3:"Ethereum 1.0 блоков. На правом стороне сайта вы можете найти предположительноё время genesis (основано на степень получених депозитов).",
+            p3_title:"Обследовай и Управляй",
+            p3_text_1:"Как только beacon chain достигнет genesis вы сможете исследовать",
+            p3_text_2:"эпохи",
+            p3_text_3:"слоты",
+            p3_text_4:"и смотреть",
+            p3_text_5:"статистики",
+            p3_text_6:"Вы можете отслеживать эффективность ваших валидаторов с помощью нашего",
+            p3_text_7:"dashboard",
+            p3_text_8:"",
+            p3_text_9:"Вы можете зарегистрироваться",
+            p3_text_10:"и включить уведомления для вашего",
+            p3_text_11:"аккаунта",
+            p3_text_12:"Получите уведомления, если ваши валидаторы отключатся. Для дополнительной информации о beacon chain зайдите в наш",
+            p3_text_13:"информационный центр."
         }
     },
 
