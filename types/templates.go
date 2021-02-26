989760a15a211381c9d54f53437045ee89e07410
peter <1674920+peterbitfly@users.noreply.github.com>
2021-02-26 10:43:56 +0100

    fix crashbug

--- types/templates.go
+++ types/templates.go
@@ -512,6 +512,7 @@ type BlockPageAttesterSlashing struct {
 type BlockPageProposerSlashing struct {
 	BlockSlot         uint64 `db:"block_slot"`
 	BlockIndex        uint64 `db:"block_index"`
+	BlockRoot         []byte `db:"block_root" json:"block_root"`
 	ProposerIndex     uint64 `db:"proposerindex"`
 	Header1Slot       uint64 `db:"header1_slot"`
 	Header1ParentRoot []byte `db:"header1_parentroot"`
