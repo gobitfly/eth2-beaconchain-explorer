4583856c28b8f2d2d088787727e78571f56b3506
Manuel <manuel@caspari.at>
2021-04-08 13:01:13 +0200

    added widget stat call

--- types/api.go
+++ types/api.go
@@ -4,3 +4,9 @@ type ApiResponse struct {
 	Status string      `json:"status"`
 	Data   interface{} `json:"data"`
 }
+
+type WidgetResponse struct {
+	Eff       interface{} `json:"efficiency"`
+	Validator interface{} `json:"validator"`
+	Epoch     int64       `json:"epoch"`
+}
