a47051771c7ebe7825e8f1b1fdb8392111162613
Stefan <22667037+qu0b@users.noreply.github.com>
2021-06-17 09:53:33 +0200

    vat (#872)
    * office/staging branch
    
    * Pricing: add hash to api link
    
    * support vat collection
    
    * fix price_id check
    
    * remove dynamic tax
    
    * merge
    
    * automatic tax
    
    * update stripe
    
    * add exclude VAT indicator
    
    * update settings api url
    
    * Show API Usage
    
    * only show usage if there is a key

--- handlers/stripe.go
+++ handlers/stripe.go
@@ -54,16 +54,26 @@ func StripeCreateCheckoutSession(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	rq := "required"
+
+	// taxRates := utils.StripeDynamicRatesLive
+	// if strings.HasPrefix(utils.Config.Frontend.Stripe.SecretKey, "sk_test") {
+	// 	taxRates = utils.StripeDynamicRatesTest
+	// }
+
+	if req.Price != utils.Config.Frontend.Stripe.Sapphire && req.Price != utils.Config.Frontend.Stripe.Emerald && req.Price != utils.Config.Frontend.Stripe.Diamond {
+		http.Error(w, "Error invalid price item provided. Must be the price ID of Sapphire, Emerald or Diamond", http.StatusBadRequest)
+		logger.Errorf("error invalid stripe price id provided: %v, expected one of [%v, %v, %v]", req.Price, utils.Config.Frontend.Stripe.Sapphire, utils.Config.Frontend.Stripe.Emerald, utils.Config.Frontend.Stripe.Diamond)
+		return
+	}
+
+	enabled := true
+	auto := "auto"
+
 	params := &stripe.CheckoutSessionParams{
-		SuccessURL: stripe.String("https://" + utils.Config.Frontend.SiteDomain + "/user/settings"),
+		SuccessURL: stripe.String("https://" + utils.Config.Frontend.SiteDomain + "/user/settings#api"),
 		CancelURL:  stripe.String("https://" + utils.Config.Frontend.SiteDomain + "/pricing"),
 		// if the customer exists use the existing customer
-		SubscriptionData: &stripe.CheckoutSessionSubscriptionDataParams{
-			// DefaultTaxRates: stripe.StringSlice([]string{
-			// "txr_1HqcFcBiORp9oTlKnyNWVp4r",
-			// "txr_1HqdWaBiORp9oTlKkij8L6dU",
-			// }),
-		},
+		SubscriptionData: &stripe.CheckoutSessionSubscriptionDataParams{},
 
 		BillingAddressCollection: &rq,
 		CustomerEmail:            &subscription.Email,
@@ -73,16 +83,25 @@ func StripeCreateCheckoutSession(w http.ResponseWriter, r *http.Request) {
 		Mode: stripe.String(string(stripe.CheckoutSessionModeSubscription)),
 		LineItems: []*stripe.CheckoutSessionLineItemParams{
 			&stripe.CheckoutSessionLineItemParams{
-				Price:           stripe.String(req.Price),
-				Quantity:        stripe.Int64(1),
-				DynamicTaxRates: utils.StripeDynamicRatesLive,
+				Price:    stripe.String(req.Price),
+				Quantity: stripe.Int64(1),
+				// DynamicTaxRates: taxRates,
 			},
 		},
+		AutomaticTax: &stripe.CheckoutSessionAutomaticTaxParams{
+			Enabled: &enabled,
+		},
+		TaxIDCollection: &stripe.CheckoutSessionTaxIDCollectionParams{
+			Enabled: &enabled,
+		},
 	}
-
 	if subscription.CustomerID != nil {
 		params.CustomerEmail = nil
 		params.Customer = subscription.CustomerID
+		params.CustomerUpdate = &stripe.CheckoutSessionCustomerUpdateParams{
+			Name:    &auto,
+			Address: &auto,
+		}
 	}
 
 	s, err := session.New(params)
@@ -407,7 +426,7 @@ func emailCustomerAboutPlanChange(email, plan string) {
 	} else if plan == utils.Config.Frontend.Stripe.Diamond {
 		p = "Diamond"
 	}
-	msg := fmt.Sprintf("You have successfully changed your payment plan to " + p + " to manage your subscription go to https://" + utils.Config.Frontend.SiteDomain + "/user/settings")
+	msg := fmt.Sprintf("You have successfully changed your payment plan to " + p + " to manage your subscription go to https://" + utils.Config.Frontend.SiteDomain + "/user/settings#api")
 	// escape html
 	msg = template.HTMLEscapeString(msg)
 	err := mail.SendMail(email, "Payment Plan Change", msg, []types.EmailAttachment{})
