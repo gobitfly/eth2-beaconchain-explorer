ad26e72aa85aecc7165290cf077cec2a1a09a6b7
vahe <vahegian@gmail.com>
2021-07-12 12:59:35 +0200

    add tag updater

---
+++ exporter/pool_tags.go
@@ -0,0 +1,32 @@
+package exporter
+
+import (
+	"eth2-exporter/db"
+	"time"
+)
+
+func UpdatePoolValidatorsTag() {
+	for true {
+		tx, err := db.DB.Beginx()
+		if err != nil {
+			logger.WithError(err).Error("Error connecting to DB")
+			// return err
+		}
+		_, err = tx.Exec(`INSERT INTO validator_tags (publickey, tag)
+		SELECT publickey, FORMAT('%s:%s', sps.name, sps.category) tag
+		FROM eth1_deposits
+		inner join stake_pools_stats as sps on ENCODE(from_address::bytea, 'hex')=sps.address
+		ON CONFLICT (publickey, tag) DO NOTHING;`)
+		if err != nil {
+			logger.WithError(err).Error("Error updating validator_tags")
+			// return err
+		}
+
+		err = tx.Commit()
+		if err != nil {
+			logger.WithError(err).Error("Error commiting transaction")
+		}
+		tx.Rollback()
+		time.Sleep(time.Minute * 5)
+	}
+}
