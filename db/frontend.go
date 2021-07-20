53b0a31ee0bbaa62ed9981af06fead4c98ac7a84
Manuel <manuel@caspari.at>
2021-07-20 14:37:26 +0200

    ram notifications

--- db/frontend.go
+++ db/frontend.go
@@ -558,16 +558,16 @@ func NewTransaction() (*sql.Tx, error) {
 
 func getMachineStatsGap(resultCount uint64) int {
 	if resultCount > 20160 { // more than 14 (31)
-		return 6
+		return 7
 	}
 	if resultCount > 10080 { // more than 7 (14)
-		return 5
+		return 6
 	}
 	if resultCount > 2880 { // more than 2 (7)
-		return 4
+		return 5
 	}
 	if resultCount > 1440 { // more than 1 (2)
-		return 3
+		return 4
 	}
 	if resultCount > 770 { // more than 12h
 		return 2
