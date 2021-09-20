0ae17e0e0cd359f7a01458f093e68c28204edd54
vahe <vahegian@gmail.com>
2021-09-20 14:04:25 +0200

    add select count query

--- handlers/eth2Deposits.go
+++ handlers/eth2Deposits.go
@@ -78,7 +78,7 @@ func Eth2DepositsData(w http.ResponseWriter, r *http.Request) {
 
 	orderDir := q.Get("order[0][dir]")
 
-	depositCount, err := db.GetEth2DepositsCount()
+	depositCount, err := db.GetEth2DepositsCount(search)
 	if err != nil {
 		logger.Errorf("error retrieving eth2_deposit count: %v", err)
 		http.Error(w, "Internal server error", 503)
