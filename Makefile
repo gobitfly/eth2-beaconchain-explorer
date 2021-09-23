c6e8338496672aa31fa9903e998636b93badfba6
madalin0b <83428650+madalin0b@users.noreply.github.com>
2021-09-23 17:23:09 +0200

    Rocketpool (new Rocketpool page and new Rocketpool tab in validator page) (#1062)
    * code cleanup
    
    * code cleanup
    
    * setup
    
    * redesign header section of page
    
    * income card redesign
    
    * add new css files
    
    * merge master
    
    * add reminder for todo
    
    * change info icon for tooltip in income section
    
    * rocketpool wip
    
    * add rank and status to headline and redesign of first card in progress
    
    * rocketpool cleanup
    
    * rocketpool validator tagging
    
    * rocketpool frontend wip
    
    * first card redesign in progress
    
    * small fixes
    
    * rocketpool frontend wip
    
    * page redesign in progress
    
    * small fix income card
    
    * fix tooltip placement for elements on banner left
    
    * rocketpool wip
    
    * rocketpool wip
    
    * add copy button validators page table
    
    * add copy button to format public key function
    
    * add copy button
    
    * add copy button in progress
    
    * copy button added
    
    * reduce button padding
    
    * small fixes
    
    * copy button positioning fix
    
    * improve layout
    
    * improve copy button position
    
    * use copy btn func
    
    * improve page layout
    
    * replace copy button
    
    * improve tables layout
    
    * format table headers
    
    * refactoring and add copy button where missing
    
    * refactor format hash function
    
    * remove unused function
    
    * fix eth1 tx links
    
    * add copy button to credentials and signature on eth2 page
    
    * refactoring
    
    * refactoring
    
    * check validator history table missing data
    
    * fix copy button behavior on eth2 deposits, poap and validator leaderboard pages
    
    * fix tooltip behavior on validator leaderboard, eth2 deposits and poap pages tables
    
    * refactoring and improving layout
    
    * add column defs to datatables functions
    
    * format table data wip
    
    * format table data wip
    
    * format table data wip
    
    * page redesign wip
    
    * fix merge master
    
    * style tags
    
    * refactoring
    
    * update min css files
    
    * update datatables language
    
    * add datatables responsive feature
    
    * style tables info, length and expand symbol
    
    * always show column with button in proposals table
    
    * format rpl columns values
    
    * reset tables pagination type
    
    * add new datatables files
    
    * add responsive feature to tables
    
    * update validator page
    
    update validator page
    
    update validator page
    
    update validator page
    
    update validator page
    
    update validator page
    
    update validator page
    
    update validator page
    
    update validator page
    
    update validator page
    
    update validator page
    
    update validator page
    
    * format tags
    
    * format tx hash icon spacing
    
    * add responsive feature to tables
    
    * fix responsive feature to tables
    
    * style tables paging and tags
    
    * small fixes
    
    * small fix
    
    * small fixes and merge tabs
    
    * add layout for rocketpool tab
    
    * small fixes
    
    * fix responsive feature in tables
    
    * fix color variables
    
    * format numbers in rocketpool tab
    
    * format stake number in rocketpool tab
    
    * small fix
    
    * small fixes badges landing page
    
    * layout fix and show tags text conditionally in heading
    
    * show rocketpool nav link conditionally
    
    * change color variable
    
    * small fixes
    
    * format rpl values rocketpool tab
    
    * alignment and color fixes
    
    * stop event propagation in column with expand button
    
    * final fixes
    
    * reset to current ui
    
    * show new tab conditionally
    
    * show rocketpool info conditionally
    
    * add logic for switch between charts in tab
    
    * add new charts tab link
    
    * show charts in one tab
    
    * layout fixes and format values in rocketpool tab
    
    * link rocketpool tag to rocketpool page
    
    * update css min files
    
    Co-authored-by: Patrick Pfeiffer <patrick@bitfly.at>
    Co-authored-by: Stefan <stefan@bitfly.at>

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ BUILDDATE=`date -u +"%Y-%m-%dT%H:%M:%S%:z"`
 PACKAGE=eth2-exporter
 LDFLAGS="-X ${PACKAGE}/version.Version=${VERSION} -X ${PACKAGE}/version.BuildDate=${BUILDDATE} -X ${PACKAGE}/version.GitCommit=${GITCOMMIT} -X ${PACKAGE}/version.GitDate=${GITDATE}"
 
-all: explorer chartshotter stats
+all: explorer stats
 
 lint:
 	golint ./...
--- types/templates.go
+++ types/templates.go
@@ -325,6 +325,21 @@ type ValidatorPageData struct {
 	InclusionDelay                      int64
 	CurrentAttestationStreak            uint64
 	LongestAttestationStreak            uint64
+	IsRocketpool                        bool
+	Rocketpool                          *RocketpoolValidatorPageData
+}
+
+type RocketpoolValidatorPageData struct {
+	NodeAddress          *[]byte    `db:"node_address"`
+	MinipoolAddress      *[]byte    `db:"minipool_address"`
+	MinipoolNodeFee      *float64   `db:"minipool_node_fee"`
+	MinipoolDepositType  *string    `db:"minipool_deposit_type"`
+	MinipoolStatus       *string    `db:"minipool_status"`
+	MinipoolStatusTime   *time.Time `db:"minipool_status_time"`
+	NodeTimezoneLocation *string    `db:"node_timezone_location"`
+	NodeRPLStake         *string    `db:"node_rpl_stake"`
+	NodeMinRPLStake      *string    `db:"node_min_rpl_stake"`
+	NodeMaxRPLStake      *string    `db:"node_max_rpl_stake"`
 }
 
 type ValidatorStatsTablePageData struct {
@@ -1056,4 +1071,62 @@ type ApiStatistics struct {
 	Monthly    *int `db:"monthly"`
 	MaxDaily   *int
 	MaxMonthly *int
+}
+
+type RocketpoolPageData struct{}
+type RocketpoolPageDataMinipool struct {
+	TotalCount               uint64    `db:"total_count"`
+	RocketpoolStorageAddress []byte    `db:"rocketpool_storage_address"`
+	ValidatorName            string    `db:"validator_name"`
+	Address                  []byte    `db:"address"`
+	Pubkey                   []byte    `db:"pubkey"`
+	NodeAddress              []byte    `db:"node_address"`
+	NodeFee                  float64   `db:"node_fee"`
+	DepositType              string    `db:"deposit_type"`
+	Status                   string    `db:"status"`
+	StatusTime               time.Time `db:"status_time"`
+}
+
+type RocketpoolPageDataNode struct {
+	TotalCount               uint64 `db:"total_count"`
+	RocketpoolStorageAddress []byte `db:"rocketpool_storage_address"`
+	Address                  []byte `db:"address"`
+	TimezoneLocation         string `db:"timezone_location"`
+	RPLStake                 string `db:"rpl_stake"`
+	MinRPLStake              string `db:"min_rpl_stake"`
+	MaxRPLStake              string `db:"max_rpl_stake"`
+}
+
+type RocketpoolPageDataDAOProposal struct {
+	TotalCount               uint64    `db:"total_count"`
+	RocketpoolStorageAddress []byte    `db:"rocketpool_storage_address"`
+	ID                       uint64    `db:"id"`
+	DAO                      string    `db:"dao"`
+	ProposerAddress          []byte    `db:"proposer_address"`
+	Message                  string    `db:"message"`
+	CreatedTime              time.Time `db:"created_time"`
+	StartTime                time.Time `db:"start_time"`
+	EndTime                  time.Time `db:"end_time"`
+	ExpiryTime               time.Time `db:"expiry_time"`
+	VotesRequired            float64   `db:"votes_required"`
+	VotesFor                 float64   `db:"votes_for"`
+	VotesAgainst             float64   `db:"votes_against"`
+	MemberVoted              bool      `db:"member_voted"`
+	MemberSupported          bool      `db:"member_supported"`
+	IsCancelled              bool      `db:"is_cancelled"`
+	IsExecuted               bool      `db:"is_executed"`
+	Payload                  []byte    `db:"payload"`
+	State                    string    `db:"state"`
+}
+
+type RocketpoolPageDataDAOMember struct {
+	TotalCount               uint64    `db:"total_count"`
+	RocketpoolStorageAddress []byte    `db:"rocketpool_storage_address"`
+	Address                  []byte    `db:"address"`
+	ID                       string    `db:"id"`
+	URL                      string    `url:"url"`
+	JoinedTime               time.Time `db:"joined_time"`
+	LastProposalTime         time.Time `db:"last_proposal_time"`
+	RPLBondAmount            string    `db:"rpl_bond_amount"`
+	UnbondedValidatorCount   uint64    `db:"unbonded_validator_count"`
 }
