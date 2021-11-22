8357eeca747c58784b6c5e5fc4acfb4916c82457
Patrick Pfeiffer <patrick@bitfly.at>
2021-11-22 18:30:22 +0100

    block-page: paginate votes

--- cmd/explorer/main.go
+++ cmd/explorer/main.go
@@ -219,6 +219,7 @@ func main() {
 			router.HandleFunc("/index/data", handlers.IndexPageData).Methods("GET")
 			router.HandleFunc("/block/{slotOrHash}", handlers.Block).Methods("GET")
 			router.HandleFunc("/block/{slotOrHash}/deposits", handlers.BlockDepositData).Methods("GET")
+			router.HandleFunc("/block/{slotOrHash}/votes", handlers.BlockVoteData).Methods("GET")
 			router.HandleFunc("/blocks", handlers.Blocks).Methods("GET")
 			router.HandleFunc("/blocks/data", handlers.BlocksData).Methods("GET")
 			router.HandleFunc("/vis", handlers.Vis).Methods("GET")
