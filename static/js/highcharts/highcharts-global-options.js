8ca18fb900d3519173255e4e7c951c5ba75001fc
Stefan <22667037+qu0b@users.noreply.github.com>
2022-06-30 15:28:46 +0200

    Adds support for lint and format checking (#1406)
    * initial lint and format commit
    
    * merge staging by hand
    
    * remove semicolon
    
    * adjust eslint rules

--- static/js/highcharts/highcharts-global-options.js
+++ static/js/highcharts/highcharts-global-options.js
@@ -1,168 +1,166 @@
 // Highcharts global options
 Highcharts.setOptions({
-    time: {
-      useUTC: false,
+  time: {
+    useUTC: false,
+  },
+  credits: {
+    enabled: true,
+    href: "https://beaconcha.in",
+    text: "beaconcha.in",
+    style: {
+      color: "var(--body-color)",
     },
-    credits: {
-      enabled: true,
-      href: 'https://beaconcha.in',
-      text: 'beaconcha.in',
+  },
+  exporting: {
+    scale: 1,
+    enabled: false,
+  },
+  title: {
+    style: {
+      color: "var(--font-color)",
+    },
+  },
+  subtitle: {
+    style: {
+      color: "var(--font-color)",
+    },
+  },
+  chart: {
+    animation: false,
+    style: {
+      fontFamily: 'Helvetica Neue", Helvetica, Arial, sans-serif',
+      color: "var(--body-color)",
+      fontSize: "12px",
+    },
+    backgroundColor: "var(--bg-color)",
+  },
+  colors: ["#7cb5ec", "#90ed7d", "#f7a35c", "#D87F98", "#FFF3EC", "#A99F9F", "#2b908f", "#f45b5b", "#91e8e1", "#A0AC53", "#5F53AC", "#8085e9", "#f15c80", "#e4d354"],
+  legend: {
+    enabled: true,
+    layout: "horizontal",
+    align: "center",
+    verticalAlign: "bottom",
+    borderWidth: 0,
+    itemStyle: {
+      color: "var(--body-color)",
+      "font-size": "0.8rem",
+      // 'font-weight': 'lighter'
+    },
+    itemHoverStyle: {
+      color: "var(--primary)",
+    },
+  },
+  xAxis: {
+    ordinal: false,
+    labels: {
       style: {
-        color: 'var(--body-color)'
+        color: "var(--font-color)",
       },
     },
-    exporting: {
-      scale: 1,
-      enabled: false,
-    },
+  },
+  yAxis: {
     title: {
       style: {
-        color: 'var(--font-color)'
-      }
-    },
-    subtitle: {
-      style: {
-        color: 'var(--font-color)'
-      }
-    },
-    chart: {
-      animation: false,
-      style: {
-        fontFamily: 'Helvetica Neue", Helvetica, Arial, sans-serif',
-        color: 'var(--body-color)',
-        fontSize: '12px'
+        color: "var(--font-color)",
+        // 'font-size': '0.8rem'
       },
-      backgroundColor: 'var(--bg-color)'
     },
-    colors: ["#7cb5ec", "#90ed7d", "#f7a35c", "#D87F98", "#FFF3EC", "#A99F9F", "#2b908f", "#f45b5b",
-             "#91e8e1", "#A0AC53", "#5F53AC", "#8085e9", "#f15c80", "#e4d354"],
-    legend: {
-      enabled: true,
-      layout: 'horizontal',
-      align: 'center',
-      verticalAlign: 'bottom',
-      borderWidth: 0,
-      itemStyle: {
-        color: 'var(--body-color)',
-        'font-size': '0.8rem',
-        // 'font-weight': 'lighter'
+    labels: {
+      style: {
+        color: "var(--body-color)",
+        "font-size": "0.8rem",
       },
-      itemHoverStyle: {
-        color: 'var(--primary)',
-      }
+    },
+    gridLineColor: "var(--border-color-transparent)",
+  },
+  navigation: {
+    menuStyle: {
+      border: "1px solid var(--border-color-transparent)",
+      background: "var(--bg-color-nav)",
+      padding: "1px 0",
+      "box-shadow": "var(--bg-color-nav) 2px 2px 5px",
+    },
+    buttonOptions: {
+      symbolStroke: "var(--body-color)",
+      symbolFill: "var(--body-color)",
+      theme: {
+        fill: "var(--bg-color)",
+        states: {
+          hover: {
+            fill: "var(--primary)",
+          },
+          select: {
+            fill: "var(--primary)",
+          },
+        },
+      },
+    },
+    menuItemStyle: {
+      padding: "0.5em 1em",
+      color: "var(--transparent-font-color)",
+      background: "none",
+      fontSize: "11px/14px",
+      transition: "background 250ms, color 250ms",
+    },
+    menuItemHoverStyle: { background: "var(--primary)", color: "var(--font-color)" },
+  },
+  navigator: {
+    enabled: true,
+    maskFill: "var(--mask-fill-color)",
+    outlineColor: "var(--border-color)",
+    handles: {
+      backgroundColor: "var(--bg-color-nav)",
+      borderColor: "var(--transparent-font-color)",
     },
     xAxis: {
-      ordinal: false,
-      labels: {
-        style: {
-          color: 'var(--font-color)'
-        }
+      gridLineColor: "var(--border-color)",
+    },
+  },
+  scrollbar: {
+    barBackgroundColor: "var(--bg-color-nav)",
+    barBorderWidth: 0,
+    buttonArrowColor: "var(--font-color)",
+    rifleColor: "var(--dark)",
+    buttonBackgroundColor: "var(--bg-color-nav)",
+    buttonBorderColor: "var(--bg-color-transparent)",
+    trackBackgroundColor: "var(--bg-color)",
+    trackBorderColor: "var(--border-color-transparent)",
+  },
+  // responsive: {
+  //   rules: [
+  //     {
+  //       condition: {
+  //         maxWidth: 590
+  //       },
+  //       chartOptions: {
+  //         chart: {
+  //           marginRight: 80
+  //         },
+  //         yAxis: [
+  //           {
+  //             title: {
+  //               text: null
+  //             }
+  //           },
+  //           {
+  //             title: {
+  //               text: null
+  //             }
+  //           }
+  //         ]
+  //       }
+  //     }
+  //   ]
+  // },
+  plotOptions: {
+    line: {
+      animation: false,
+      lineWidth: 2.5,
+    },
+    column: {
+      dataGrouping: {
+        approximation: "sum",
       },
     },
-    yAxis: {
-      title: {
-        style: {
-          color: 'var(--font-color)',
-          // 'font-size': '0.8rem'
-        }
-      },
-      labels: {
-        style: {
-          color: 'var(--body-color)',
-          'font-size': '0.8rem'
-        }
-      },
-      gridLineColor: 'var(--border-color-transparent)',
-    },
-    navigation: {
-      menuStyle: {
-        "border": "1px solid var(--border-color-transparent)",
-        "background": "var(--bg-color-nav)",
-        "padding": "1px 0",
-        "box-shadow": "var(--bg-color-nav) 2px 2px 5px"
-      },
-      buttonOptions: {
-        symbolStroke: 'var(--body-color)',
-        symbolFill: 'var(--body-color)',
-        theme: {
-          fill: 'var(--bg-color)',
-          states: {
-            hover: {
-              fill: 'var(--primary)'
-            },
-            select: {
-              fill: 'var(--primary)'
-            }
-          }
-        }
-      },
-      menuItemStyle: {
-        "padding": "0.5em 1em",
-        "color": "var(--transparent-font-color)",
-        "background": "none",
-        "fontSize": "11px/14px",
-        "transition": "background 250ms, color 250ms"
-      },
-      menuItemHoverStyle: {"background": "var(--primary)", "color": "var(--font-color)"}
-    },
-    navigator: {
-      enabled: true,
-      maskFill: 'var(--mask-fill-color)',
-      outlineColor: 'var(--border-color)',
-      handles: {
-        backgroundColor: 'var(--bg-color-nav)',
-        borderColor: 'var(--transparent-font-color)',
-      },
-      xAxis: {
-        gridLineColor: 'var(--border-color)',
-      },
-    },
-    scrollbar: {
-      barBackgroundColor: 'var(--bg-color-nav)',
-      barBorderWidth: 0,
-      buttonArrowColor: 'var(--font-color)',
-      rifleColor: 'var(--dark)',
-      buttonBackgroundColor: 'var(--bg-color-nav)',
-      buttonBorderColor: 'var(--bg-color-transparent)',
-      trackBackgroundColor: 'var(--bg-color)',
-      trackBorderColor: 'var(--border-color-transparent)',
-    },
-    // responsive: {
-    //   rules: [
-    //     {
-    //       condition: {
-    //         maxWidth: 590
-    //       },
-    //       chartOptions: {
-    //         chart: {
-    //           marginRight: 80
-    //         },
-    //         yAxis: [
-    //           {
-    //             title: {
-    //               text: null
-    //             }
-    //           },
-    //           {
-    //             title: {
-    //               text: null
-    //             }
-    //           }
-    //         ]
-    //       }
-    //     }
-    //   ]
-    // },
-    plotOptions: {
-      line: {
-        animation: false,
-        lineWidth: 2.5
-      },
-      column: {
-        dataGrouping: {
-          approximation: 'sum'
-        }
-      }
-    }
-  })
-  
+  },
+})
--- static/js/payment.js
+++ static/js/payment.js
@@ -1,32 +1,31 @@
-  // In production, this should check CSRF, and not pass the session ID.
-  // The customer ID for the portal should be pulled from the 
-  // authenticated user on the server.
-  var manageBillingForm = document.querySelectorAll('.manage-billing-form');
-  for (let i = 0; i < manageBillingForm.length; i++) {
-    manageBillingForm[i].addEventListener('submit', function(e) {
-      e.preventDefault();
-      var token = ""
-      if (document.getElementsByName("CsrfField") && document.getElementsByName("CsrfField").length) {
-        token = document.getElementsByName("CsrfField")[0].value
-      }
-      fetch('/user/stripe/customer-portal', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRF-Token': token,
-        },
-        credentials: 'include',
-        body: JSON.stringify({
-          returnURL: window.location.href
-        }),
+// In production, this should check CSRF, and not pass the session ID.
+// The customer ID for the portal should be pulled from the
+// authenticated user on the server.
+var manageBillingForm = document.querySelectorAll(".manage-billing-form")
+for (let i = 0; i < manageBillingForm.length; i++) {
+  manageBillingForm[i].addEventListener("submit", function (e) {
+    e.preventDefault()
+    var token = ""
+    if (document.getElementsByName("CsrfField") && document.getElementsByName("CsrfField").length) {
+      token = document.getElementsByName("CsrfField")[0].value
+    }
+    fetch("/user/stripe/customer-portal", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": token,
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        returnURL: window.location.href,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        window.location.href = data.url
       })
-        .then((response) => response.json())
-        .then((data) => {
-          window.location.href = data.url;
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-    });
-  }
-
+      .catch((error) => {
+        console.error("Error:", error)
+      })
+  })
+}
