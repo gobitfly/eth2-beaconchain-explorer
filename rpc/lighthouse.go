1d35b3fef3b8d6a376e0d793bbc4cac440132891
InvisibleBitfly <110384239+InvisibleBitfly@users.noreply.github.com>
2022-08-05 14:56:41 +0200

    exporter: fix: turn uint into int to prevent underflowing it (#1466)
    * exporter: fix: turn uint into int to prevent underflowing it
    
    * fix: use an uint, check if its 0 instead

--- rpc/lighthouse.go
+++ rpc/lighthouse.go
@@ -797,7 +797,7 @@ func syncCommitteeParticipation(bits []byte) float64 {
 
 // GetValidatorParticipation will get the validator participation from the Lighthouse RPC api
 func (lc *LighthouseClient) GetValidatorParticipation(epoch uint64) (*types.ValidatorParticipation, error) {
-	if epoch >= LighthouseLatestHeadEpoch-1 {
+	if LighthouseLatestHeadEpoch == 0 || epoch >= LighthouseLatestHeadEpoch-1 {
 		// update LighthouseLatestHeadEpoch to make sure we are continuing with the latest data
 		// we need to check when epoch = head and epoch head - 1 so our following logic acts correctly when we are close to the head
 		head, err := lc.GetChainHead()
