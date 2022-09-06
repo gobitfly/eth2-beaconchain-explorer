7d41ddcbdf0a9cf3e550281c2133ee12f0b6cc02
recy21 <stefan.fuhrmann@bitfly.at>
2022-09-06 19:18:37 +0200

    #BIDS-436 Implement Block Table Page

--- handlers/eth1Blocks.go
+++ handlers/eth1Blocks.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"html/template"
 	"math/big"
+	"math/rand"
 	"net/http"
 	"strconv"
 )
@@ -91,33 +92,88 @@ func GetEth1BlocksTableData(draw, start, length uint64) (*types.DataTableRespons
 
 	tableData := make([][]interface{}, len(blocks))
 	for i, b := range blocks {
-		ts := uint64(b.GetTime().AsTime().Unix())
+		/* // #RECY #TODO add to GetBlocksDescending?
+		fullBlockData, err := db.BigtableClient.GetBlockFromBlocksTable(b.GetNumber())
+		if err != nil {
+			return nil, err
+		}
+
+		logrus.Infof("%v %v", b.GetTransactionCount(), len(fullBlockData.Transactions))
+		tTypes := make([]int, 100)
+		for _, v := range fullBlockData.Transactions {
+			tTypes[v.Type] += 1
+		}
+		for k, v := range tTypes {
+			if v != 0 {
+				logrus.Infof(">> %d %d", k, v)
+			}
+		} /**/
+
+		posActive := true
 		slotText := "-"
 		epochText := "-"
-		if ts >= utils.Config.Chain.GenesisTimestamp {
-			// slot
-			slot := (ts - utils.Config.Chain.GenesisTimestamp) / utils.Config.Chain.Config.SecondsPerSlot
-			slotText = fmt.Sprintf(`<A href="block/%d">%s</A>`, b.GetNumber(), utils.FormatAddCommas(slot))
-			// epoch
-			{
-				epoch := slot / utils.Config.Chain.Config.SlotsPerEpoch
-				epochText = fmt.Sprintf(`<A href="epoch/%d">%s</A>`, epoch, utils.FormatAddCommas(epoch))
+		{
+			// Difficulty == 0 represent active staking, so we will show the slot
+			for _, v := range b.GetDifficulty() {
+				if v != 0 {
+					posActive = false
+					break
+				}
+			}
+
+			if posActive {
+				ts := uint64(b.GetTime().AsTime().Unix())
+				if ts >= utils.Config.Chain.GenesisTimestamp {
+					// slot
+					slot := (ts - utils.Config.Chain.GenesisTimestamp) / utils.Config.Chain.Config.SecondsPerSlot
+					slotText = fmt.Sprintf(`<A href="block/%d">%s</A>`, b.GetNumber(), utils.FormatAddCommas(slot))
+
+					// epoch
+					{
+						epoch := slot / utils.Config.Chain.Config.SlotsPerEpoch
+						epochText = fmt.Sprintf(`<A href="epoch/%d">%s</A>`, epoch, utils.FormatAddCommas(epoch))
+					}
+				}
 			}
 		}
 
-		reward := new(big.Int).Add(utils.BlockReward(b.GetNumber()), new(big.Int).SetBytes(b.GetTxReward()))
+		// #RECY #RANDOM
+		randomPropserName := ""
+		switch os := rand.Intn(20); os {
+		case 0:
+			randomPropserName = "gabuwhale"
+		case 1:
+			randomPropserName = "Uma-70"
+		case 2:
+			randomPropserName = "Untitled"
+		}
+
+		gasHalf := float64(b.GetGasLimit()) / 2.0
+		blockReward := utils.BlockReward(b.GetNumber())
+		txReward := new(big.Int).Sub(new(big.Int).SetBytes(b.GetTxReward()), new(big.Int).Mul(new(big.Int).SetBytes(b.GetBaseFee()), big.NewInt(int64(b.GetTransactionCount()))))
+		totalReward := new(big.Int).Add(blockReward, new(big.Int).Add(txReward, new(big.Int).SetBytes(b.GetUncleReward())))
+		burned := new(big.Int).Mul(new(big.Int).SetBytes(b.GetBaseFee()), big.NewInt(int64(b.GetGasUsed())))
+
+		burnedPercentage := float64(0.0)
+		if len(txReward.Bits()) != 0 {
+			txBurnedBig := new(big.Float).SetInt(burned)
+			txBurnedBig.Quo(txBurnedBig, new(big.Float).SetInt(txReward))
+			burnedPercentage, _ = txBurnedBig.Float64()
+		}
+
 		tableData[i] = []interface{}{
 			epochText, // Epoch
 			fmt.Sprintf(`%s<BR /><font style="font-size: .63rem; color: grey;">%v</font>`, slotText, utils.FormatTimestamp(b.GetTime().AsTime().Unix())), // Slot
-			fmt.Sprintf(`<A href="block/%d">%v</A>`, b.GetNumber(), utils.FormatAddCommas(b.GetNumber())),                                                // Block // utils.FormatBlockNumber(b.GetNumber()),
-			"-",                     // Status
-			"-",                     // Proposer
-			b.GetTransactionCount(), // Transactions
-			fmt.Sprintf(`%v<BR /><font style="font-size: .63rem; color: grey;">%.2f%% + -%%</font>`, utils.FormatAddCommas(b.GetGasUsed()), float64(b.GetGasUsed())/float64(b.GetGasLimit())*100.0), // Gas Used
+			fmt.Sprintf(`<A href="block/%d">%v</A>`, b.GetNumber(), utils.FormatAddCommas(b.GetNumber())),                                                // Block
+			utils.FormatBlockStatus(uint64(rand.Intn(4))),                       // Status #RECY #RANDOM
+			fmt.Sprintf("%x", b.GetCoinbase()),                                  // Coinbase
+			utils.FormatValidatorWithName(rand.Intn(400000), randomPropserName), // Proposer #RECY #RANDOM
+			b.GetTransactionCount(),                                             // Transactions
+			fmt.Sprintf(`%v<BR /><span data-toggle="tooltip" data-placement="top" title="Gas Used %%" style="font-size: .63rem; color: grey;">%.2f%%</span>&nbsp;<span data-toggle="tooltip" data-placement="top" title="%% of Gas Target" style="font-size: .63rem; color: grey;">(%+.2f%%)</span>`, utils.FormatAddCommas(b.GetGasUsed()), float64(b.GetGasUsed())/float64(b.GetGasLimit())*100.0, ((float64(b.GetGasUsed())-gasHalf)/gasHalf)*100.0), // Gas Used
 			utils.FormatAddCommas(b.GetGasLimit()),                               // Gas Limit
 			utils.FormatAmount(new(big.Int).SetBytes(b.GetBaseFee()), "GWei", 2), // Base Fee
-			utils.FormatAmount(reward, "ETH", 5),                                 // Reward
-			fmt.Sprintf(`%v (%.2f%%)`, utils.FormatAmount(new(big.Int).Mul(new(big.Int).SetBytes(b.GetBaseFee()), big.NewInt(int64(b.GetGasUsed()))), "ETH", 5), 0.0), // Burned Fees
+			utils.FormatAmount(totalReward, "ETH", 5),                            // Reward
+			fmt.Sprintf(`%v<BR /><font style="font-size: .63rem; color: grey;">%.2f%%</font>`, utils.FormatAmount(burned, "ETH", 5), burnedPercentage*100.0), // Burned Fees
 		}
 	}
 
