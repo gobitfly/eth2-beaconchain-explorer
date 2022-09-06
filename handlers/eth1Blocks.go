3b6feca218834efb66d679d49ccd7d41f13fa5eb
recy21 <stefan.fuhrmann@bitfly.at>
2022-09-06 11:54:36 +0200

    #BIDS-436 Implement Block Table Page

--- handlers/eth1Blocks.go
+++ handlers/eth1Blocks.go
@@ -74,6 +74,16 @@ func Eth1BlocksData(w http.ResponseWriter, r *http.Request) {
 func GetEth1BlocksTableData(draw, start, length uint64) (*types.DataTableResponse, error) {
 	latestBlockNumber := services.LatestEth1BlockNumber()
 
+	if start > latestBlockNumber {
+		start = 1
+	} else {
+		start = latestBlockNumber - start
+	}
+
+	if length > start {
+		length = start
+	}
+
 	blocks, err := db.BigtableClient.GetBlocksDescending(start, length)
 	if err != nil {
 		return nil, err
@@ -81,19 +91,33 @@ func GetEth1BlocksTableData(draw, start, length uint64) (*types.DataTableRespons
 
 	tableData := make([][]interface{}, len(blocks))
 	for i, b := range blocks {
+		ts := uint64(b.GetTime().AsTime().Unix())
+		slotText := "-"
+		epochText := "-"
+		if ts >= utils.Config.Chain.GenesisTimestamp {
+			// slot
+			slot := (ts - utils.Config.Chain.GenesisTimestamp) / utils.Config.Chain.Config.SecondsPerSlot
+			slotText = fmt.Sprintf(`<A href="block/%d">%s</A>`, b.GetNumber(), utils.FormatAddCommas(slot))
+			// epoch
+			{
+				epoch := slot / utils.Config.Chain.Config.SlotsPerEpoch
+				epochText = fmt.Sprintf(`<A href="epoch/%d">%s</A>`, epoch, utils.FormatAddCommas(epoch))
+			}
+		}
+
 		reward := new(big.Int).Add(utils.BlockReward(b.GetNumber()), new(big.Int).SetBytes(b.GetTxReward()))
 		tableData[i] = []interface{}{
-			"-", // Epoch
-			fmt.Sprintf("-<BR /><font style=\"font-size: .63rem; color: grey;\">%v</font>", utils.FormatTimestamp(b.GetTime().AsTime().Unix())), // Slot
-			fmt.Sprintf("<A href=block/%d>%d</A>", b.GetNumber(), b.GetNumber()),                                                                // Block // utils.FormatBlockNumber(b.GetNumber()),
-			"-", // Status
-			utils.FormatAmount(new(big.Int).SetBytes(b.GetTxReward()), "-", 5), // Proposer
+			epochText, // Epoch
+			fmt.Sprintf(`%s<BR /><font style="font-size: .63rem; color: grey;">%v</font>`, slotText, utils.FormatTimestamp(b.GetTime().AsTime().Unix())), // Slot
+			fmt.Sprintf(`<A href="block/%d">%v</A>`, b.GetNumber(), utils.FormatAddCommas(b.GetNumber())),                                                // Block // utils.FormatBlockNumber(b.GetNumber()),
+			"-",                     // Status
+			"-",                     // Proposer
 			b.GetTransactionCount(), // Transactions
-			fmt.Sprintf("%v<BR /><font style=\"font-size: .63rem; color: grey;\">%.2f%% + -%%</font>", utils.FormatAddCommas(b.GetGasUsed()), float64(b.GetGasUsed())/float64(b.GetGasLimit())*100.0), // Gas Used
+			fmt.Sprintf(`%v<BR /><font style="font-size: .63rem; color: grey;">%.2f%% + -%%</font>`, utils.FormatAddCommas(b.GetGasUsed()), float64(b.GetGasUsed())/float64(b.GetGasLimit())*100.0), // Gas Used
 			utils.FormatAddCommas(b.GetGasLimit()),                               // Gas Limit
 			utils.FormatAmount(new(big.Int).SetBytes(b.GetBaseFee()), "GWei", 2), // Base Fee
 			utils.FormatAmount(reward, "ETH", 5),                                 // Reward
-			fmt.Sprintf("%v (%.2f%%)", utils.FormatAmount(new(big.Int).Mul(new(big.Int).SetBytes(b.GetBaseFee()), big.NewInt(int64(b.GetGasUsed()))), "ETH", 5), 0.0), // Burned Fees
+			fmt.Sprintf(`%v (%.2f%%)`, utils.FormatAmount(new(big.Int).Mul(new(big.Int).SetBytes(b.GetBaseFee()), big.NewInt(int64(b.GetGasUsed()))), "ETH", 5), 0.0), // Burned Fees
 		}
 	}
 
