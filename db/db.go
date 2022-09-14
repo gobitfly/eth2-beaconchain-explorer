06c140112057c5ffa0921b371590fcd0e539e5ed
peter <1674920+peterbitfly@users.noreply.github.com>
2022-09-14 15:30:47 +0200

    re-add local in memory object cache

--- db/db.go
+++ db/db.go
@@ -48,14 +48,12 @@ func MustInitRedisCache(address string) {
 	gocacheClient := gocache.New(time.Hour, time.Minute)
 	gocacheStore := store2.NewGoCache(gocacheClient)
 
-	caches := []cache2.SetterCacheInterface[any]{}
+	caches := []cache2.SetterCacheInterface[any]{cache2.New[any](gocacheStore)}
 	if !utils.Config.Frontend.Debug {
 		redisStore := store2.NewRedis(redis.NewClient(&redis.Options{
 			Addr: address,
 		}))
 		caches = append(caches, cache2.New[any](redisStore))
-	} else {
-		caches = append(caches, cache2.New[any](gocacheStore))
 	}
 
 	cacheManager := cache2.NewChain(
