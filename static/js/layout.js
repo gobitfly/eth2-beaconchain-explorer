6b8def3c594e40f8c7936271ea48a5fd4a6a89c4
Lucca <lucca.dukic@bitfly.at>
2022-09-19 09:29:53 +0200

    display readable block extra in post merge blocks

--- static/js/layout.js
+++ static/js/layout.js
@@ -115,6 +115,31 @@ function copyTs() {
   }
 }
 
+function viewHexDataAs(id, type) {
+  var extraDataHex = $(`#${id}`).attr("aria-hex-data")
+  if (!extraDataHex) {
+    return
+  }
+
+  if (type === "hex") {
+    $(`#${id}`).text(extraDataHex)
+  } else {
+    try {
+      var r = decodeURIComponent(extraDataHex.replace(/\s+/g, "").replace(/[0-9a-f]{2}/g, "%$&"))
+      $(`#${id}`).text(r.replace("0x", ""))
+    } catch (e) {
+      $(`#${id}`).text(hex2a(extraDataHex.replace("0x", "")))
+    }
+  }
+}
+
+function hex2a(hexx) {
+  var hex = hexx.toString() //force conversion
+  var str = ""
+  for (var i = 0; i < hex.length; i += 2) str += String.fromCharCode(parseInt(hex.substr(i, 2), 16))
+  return str
+}
+
 // typeahead
 $(document).ready(function () {
   formatTimestamps() // make sure this happens before tooltips
