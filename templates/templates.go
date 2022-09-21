7795fb2f5f436b757c2fa25f03646bda3884ae97
peter <1674920+peterbitfly@users.noreply.github.com>
2022-09-21 12:02:10 +0200

    add scaffolding for dynamic template loading

--- templates/templates.go
+++ templates/templates.go
@@ -8,3 +8,30 @@ var (
 	//go:embed *
 	Files embed.FS
 )
+
+// var templateCache = make(map[string]*template.Template)
+// var templateCacheMux = &sync.RWMutex{}
+
+// func GetTemplate(name string, files ...string) *template.Template {
+// 	if utils.Config.Frontend.Debug {
+// 		for i := range files {
+// 			files[i] = "templates/" + files[i]
+// 		}
+// 		return template.Must(template.New(name).Funcs(utils.GetTemplateFuncs()).ParseFiles(files...))
+// 	}
+
+// 	name = name + strings.Join(files, "-")
+
+// 	templateCacheMux.RLock()
+// 	defer templateCacheMux.RUnlock()
+// 	if templateCache[name] != nil {
+// 		return templateCache[name]
+// 	}
+
+// 	tmpl := template.Must(template.New(name).Funcs(utils.GetTemplateFuncs()).ParseFS(templates.Files, files...))
+// 	templateCacheMux.Lock()
+// 	templateCache[name] = tmpl
+// 	templateCacheMux.Unlock()
+
+// 	return templateCache[name]
+// }
