3e6f9f897e08268088909efc9113db7dfcf24b39
Patrick Pfeiffer <patrick@bitfly.at>
2022-10-06 18:20:23 +0000

    workshop

--- cmd/eth1indexer/main.go
+++ cmd/eth1indexer/main.go
@@ -58,6 +58,9 @@ func main() {
 	tokenPriceExportList := flag.String("token.price.list", "", "Tokenlist path to use for the token price export")
 	tokenPriceExportFrequency := flag.Duration("token.price.frequency", time.Hour, "Token price export interval")
 
+	bigtableProject := flag.String("bigtable.project", "", "Bigtable project")
+	bigtableInstance := flag.String("bigtable.instance", "", "Bigtable instance")
+
 	flag.Parse()
 	if erigonEndpoint == nil || *erigonEndpoint == "" {
 		logrus.Fatal("no erigon node url provided")
@@ -89,7 +92,7 @@ func main() {
 		logrus.Fatalf("node chain id missmatch, wanted %v got %v", chainId, nodeChainId.String())
 	}
 
-	bt, err := db.InitBigtable("etherchain", "etherchain", chainId)
+	bt, err := db.InitBigtable(*bigtableProject, *bigtableInstance, chainId)
 	if err != nil {
 		logrus.Fatalf("error connecting to bigtable: %v", err)
 	}
