6797f314dac08243c00d0a0a0a5ac25a6566cb07
Stefan <stefan@bitfly.at>
2022-11-18 14:29:00 +0100

    price & data on page load (BIDS-664)

--- handlers/burn.go
+++ handlers/burn.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"eth2-exporter/price"
 	"eth2-exporter/services"
 	"eth2-exporter/templates"
 	"net/http"
@@ -17,6 +18,21 @@ func Burn(w http.ResponseWriter, r *http.Request) {
 	// data.Meta.Tdata2 = utils.FormatAmount(data.Data.(*types.BurnPageData).BurnRate24h/1e18) + " ETH/min"
 	// data.Meta.Description = "The current ethereum burn rate is " + data.Meta.Tdata2 + ". A total of " + utils.FormatUSD(data.Data.(*types.BurnPageData).TotalBurned/1e18) + "ETH with a market value of $" + data.Meta.Tdata1 + " has been burned. " + data.Meta.Description
 
+	latestBurn := services.LatestBurnData()
+
+	currency := GetCurrency(r)
+
+	if currency == "ETH" {
+		currency = "USD"
+	}
+
+	price := price.GetEthPrice(currency)
+
+	latestBurn.Price = price
+	latestBurn.Currency = currency
+
+	data.Data = latestBurn
+
 	err := burnTemplate.ExecuteTemplate(w, "layout", data)
 
 	if err != nil {
@@ -27,7 +43,20 @@ func Burn(w http.ResponseWriter, r *http.Request) {
 
 func BurnPageData(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(services.LatestBurnData())
+	latestBurn := services.LatestBurnData()
+
+	currency := GetCurrency(r)
+
+	if currency == "ETH" {
+		currency = "USD"
+	}
+
+	price := price.GetEthPrice(currency)
+
+	latestBurn.Price = price
+	latestBurn.Currency = currency
+
+	err := json.NewEncoder(w).Encode(latestBurn)
 	if err != nil {
 		logger.Errorf("error sending latest burn page data: %v", err)
 		http.Error(w, "Internal server error", 503)
