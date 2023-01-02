b5a81ef4e7b71b9d6e4ea5d03cb263b98de7f25a
recy21 <stefan.fuhrmann@bitfly.at>
2023-01-02 13:02:04 +0100

    (BIDS-1292) Remove Debug-Outputs to check theorie of "superfluous response.WriteHeader" warnings

--- handlers/eth1Blocks.go
+++ handlers/eth1Blocks.go
@@ -121,7 +121,7 @@ func getProposerAndStatusFromSlot(startSlot uint64, endSlot uint64) (map[uint64]
 func Eth1BlocksHighest(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/text")
-	logrus.Infof("<< Write / eth1Blocks.go / L-126")
+	w.Write([]byte(fmt.Sprintf("%d", services.LatestEth1BlockNumber())))
 }
 
 func getEth1BlocksTableData(draw, start, length, recordsTotal uint64) (*types.DataTableResponse, error) {
