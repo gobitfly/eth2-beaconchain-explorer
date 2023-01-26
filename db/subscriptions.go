6c3a2f6518cbe979c03961939e619c98a69f3041
peter <1674920+peterbitfly@users.noreply.github.com>
2023-01-26 12:20:46 +0100

    (BIDS-1481) source validator balances data from bigtable instead of postgres

--- db/subscriptions.go
+++ db/subscriptions.go
@@ -170,7 +170,7 @@ func GetSubscriptions(filter GetSubscriptionsFilter) ([]*types.Subscription, err
 	qry := "SELECT event_name, event_filter, last_sent_ts, last_sent_epoch, created_ts, created_epoch, event_threshold, ENCODE(unsubscribe_hash, 'hex') as unsubscribe_hash FROM users_subscriptions"
 
 	if filter.JoinValidator {
-		qry = "SELECT id, user_id, event_name, event_filter, last_sent_ts, created_ts, validators.balance as balance, ENCODE(unsubscribe_hash, 'hex') as unsubscribe_hash FROM users_subscriptions INNER JOIN validators ON users_subscriptions.event_filter = ENCODE(validators.pubkey::bytea, 'hex')"
+		qry = "SELECT id, user_id, event_name, event_filter, last_sent_ts, created_ts, ENCODE(unsubscribe_hash, 'hex') as unsubscribe_hash FROM users_subscriptions INNER JOIN validators ON users_subscriptions.event_filter = ENCODE(validators.pubkey::bytea, 'hex')"
 	}
 
 	if filter.EventNames == nil && filter.UserIDs == nil && filter.EventFilters == nil {
