71b19a6ee35bd1991dc97de11bf280b9434b138a
peter <1674920+peterbitfly@users.noreply.github.com>
2023-02-25 22:25:28 +0100

    (BIDS-1644) fix validator rewards view

--- db/statistics.go
+++ db/statistics.go
@@ -386,7 +386,8 @@ func GetValidatorIncomeHistory(validator_indices []uint64, lowerBoundDay uint64,
 	err := ReaderDb.Select(&result, `
 		SELECT 
 			day, 
-			SUM(COALESCE(cl_rewards_gwei, 0)) AS cl_rewards_gwei
+			SUM(COALESCE(cl_rewards_gwei, 0)) AS cl_rewards_gwei,
+			SUM(COALESCE(end_balance, 0)) AS end_balance
 		FROM validator_stats 
 		WHERE validatorindex = ANY($1) AND day BETWEEN $2 AND $3 
 		GROUP BY day 
@@ -396,7 +397,7 @@ func GetValidatorIncomeHistory(validator_indices []uint64, lowerBoundDay uint64,
 	// retrieve rewards for epochs not yet in stats
 	lastDay := len(result) - 1
 	currentDayIncome := int64(0)
-	if lastDay > 0 {
+	if upperBoundDay == 65536 && lastDay > 0 {
 		currentDay := uint64(lastDay + 1)
 		startEpoch := currentDay * utils.EpochsPerDay()
 		endEpoch := startEpoch + utils.EpochsPerDay() - 1
