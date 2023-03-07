811b033319be9efc3b8763396484aaae380ce194
peter <1674920+peterbitfly@users.noreply.github.com>
2023-03-07 10:54:41 +0100

    (NOBIDS) fix logging

--- cmd/explorer/main.go
+++ cmd/explorer/main.go
@@ -65,7 +65,10 @@ func main() {
 		logrus.Fatalf("error reading config file: %v", err)
 	}
 	utils.Config = cfg
-	logrus.WithField("config", *configPath).WithField("version", version.Version).WithField("chainName", utils.Config.Chain.Config.ConfigName).Printf("starting")
+	logrus.WithFields(logrus.Fields{
+		"config":    *configPath,
+		"version":   version.Version,
+		"chainName": utils.Config.Chain.Config.ConfigName}).Printf("starting")
 
 	if utils.Config.Chain.Config.SlotsPerEpoch == 0 || utils.Config.Chain.Config.SecondsPerSlot == 0 {
 		utils.LogFatal(err, "invalid chain configuration specified, you must specify the slots per epoch, seconds per slot and genesis timestamp in the config file", 0)
@@ -213,7 +216,7 @@ func main() {
 		if utils.Config.Indexer.Node.Type == "lighthouse" {
 			rpcClient, err = rpc.NewLighthouseClient("http://"+cfg.Indexer.Node.Host+":"+cfg.Indexer.Node.Port, chainID)
 			if err != nil {
-				utils.LogFatal(err, "exporting OneTimeExport epochs error", 0)
+				utils.LogFatal(err, "new explorer lighthouse client error", 0)
 			}
 		} else {
 			logrus.Fatalf("invalid note type %v specified. supported node types are prysm and lighthouse", utils.Config.Indexer.Node.Type)
@@ -225,7 +228,7 @@ func main() {
 				for _, epoch := range utils.Config.Indexer.OneTimeExport.Epochs {
 					err := exporter.ExportEpoch(epoch, rpcClient)
 					if err != nil {
-						utils.LogFatal(err, "exporting OneTimeExport start to end epoch error", 0)
+						utils.LogFatal(err, "exporting OneTimeExport epochs error", 0)
 					}
 				}
 			} else {
@@ -233,7 +236,7 @@ func main() {
 				for epoch := utils.Config.Indexer.OneTimeExport.StartEpoch; epoch <= utils.Config.Indexer.OneTimeExport.EndEpoch; epoch++ {
 					err := exporter.ExportEpoch(epoch, rpcClient)
 					if err != nil {
-						logrus.Fatal(err)
+						utils.LogFatal(err, "exporting OneTimeExport start to end epoch error", 0)
 					}
 				}
 			}
