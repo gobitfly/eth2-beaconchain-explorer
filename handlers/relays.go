81a919470cdba179ed167a0212e460193c73b720
Alexander Maushammer <alexander.maushammer@bitfly.at>
2023-03-14 08:01:08 +0100

    (NOBIDS) prevent relays template from crashing

--- handlers/relays.go
+++ handlers/relays.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"eth2-exporter/services"
 	"eth2-exporter/templates"
+	"eth2-exporter/types"
 	"net/http"
 )
 
@@ -15,7 +16,11 @@ func Relays(w http.ResponseWriter, r *http.Request) {
 
 	relayData := services.LatestRelaysPageData()
 
-	data.Data = relayData
+	if relayData == nil {
+		data.Data = types.RelaysResp{} //need to add a dummy data to prevent template to crash
+	} else {
+		data.Data = relayData
+	}
 
 	if handleTemplateError(w, r, "relays.go", "Relays", "", relaysServicesTemplate.ExecuteTemplate(w, "layout", data)) != nil {
 		return // an error has occurred and was processed
