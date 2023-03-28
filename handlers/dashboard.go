a5adcd8446df34b82227dd0346678fd7faf10b2e
D13ce <stefan.benker@bitfly.at>
2023-03-28 11:03:32 +0200

    (BIDS-1801) Add tooltip to next withdrawal amount on dashboard

--- handlers/dashboard.go
+++ handlers/dashboard.go
@@ -237,14 +237,16 @@ func getNextWithdrawalRow(queryValidators []uint64) ([][]interface{}, error) {
 			continue
 		}
 		for _, v := range validatorsDb {
-			if v.Index == balanceIndex {
-				if (balance[0].Balance > 0 && v.WithdrawableEpoch <= epoch) ||
-					(balance[0].EffectiveBalance == utils.Config.Chain.Config.MaxEffectiveBalance && balance[0].Balance > utils.Config.Chain.Config.MaxEffectiveBalance) {
-					// this validator is withdrawable, check if it is the next one
-					if nextValidator == nil || v.Index > *stats.LatestValidatorWithdrawalIndex {
-						nextValidator = v
-						nextValidator.Balance = balance[0].Balance
-					}
+			if v.Index != balanceIndex {
+				continue
+			}
+
+			if (balance[0].Balance > 0 && v.WithdrawableEpoch <= epoch) ||
+				(balance[0].EffectiveBalance == utils.Config.Chain.Config.MaxEffectiveBalance && balance[0].Balance > utils.Config.Chain.Config.MaxEffectiveBalance) {
+				// this validator is withdrawable, check if it is the next one
+				if nextValidator == nil || v.Index > *stats.LatestValidatorWithdrawalIndex {
+					nextValidator = v
+					nextValidator.Balance = balance[0].Balance
 				}
 			}
 		}
@@ -286,7 +288,7 @@ func getNextWithdrawalRow(queryValidators []uint64) ([][]interface{}, error) {
 		template.HTML(fmt.Sprintf(`<span class="text-muted">~ %s</span>`, utils.FormatBlockSlot(utils.TimeToSlot(uint64(timeToWithdrawal.Unix()))))),
 		template.HTML(fmt.Sprintf(`<span class="">~ %s</span>`, utils.FormatTimeFromNow(timeToWithdrawal))),
 		withdrawalCredentialsTemplate,
-		template.HTML(fmt.Sprintf(`<span class="text-muted">~ %s</span>`, utils.FormatAmount(new(big.Int).Mul(new(big.Int).SetUint64(nextValidator.Balance-utils.Config.Chain.Config.MaxEffectiveBalance), big.NewInt(1e9)), "ETH", 6))),
+		template.HTML(fmt.Sprintf(`<span class="text-muted"><span data-toggle="tooltip" title="If the withdrawal were to be processed at this very moment, this amount would be withdrawn"><i class="far ml-1 fa-question-circle" style="margin-left: 0px !important;"></i></span> %s</span>`, utils.FormatAmount(new(big.Int).Mul(new(big.Int).SetUint64(nextValidator.Balance-utils.Config.Chain.Config.MaxEffectiveBalance), big.NewInt(1e9)), "ETH", 6))),
 	})
 
 	return nextData, nil
