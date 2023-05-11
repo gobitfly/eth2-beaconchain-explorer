f14f79c6b513f453c15f57f09b37407a47279e39
MauserBitfly <125363940+MauserBitfly@users.noreply.github.com>
2023-05-11 09:47:56 +0200

    (BIDS-1766) no attestation found (#2200)

--- handlers/validators.go
+++ handlers/validators.go
@@ -320,7 +320,7 @@ func ValidatorsData(w http.ResponseWriter, r *http.Request) {
 			tableData[i] = append(tableData[i], nil)
 		}
 
-		if v.LastAttestationSlot != nil {
+		if v.LastAttestationSlot != nil && *v.LastAttestationSlot > 0 {
 			tableData[i] = append(tableData[i], []interface{}{
 				*v.LastAttestationSlot,
 				utils.SlotToTime(uint64(*v.LastAttestationSlot)).Unix(),
