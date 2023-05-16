9c5fc8055c8b574a6427ca7998a92ddbfc1507b4
Alexander Maushammer <alexander.maushammer@bitfly.at>
2023-05-16 08:10:36 +0200

    (NOBIDS) fix export sequenc e

--- db/statistics.go
+++ db/statistics.go
@@ -151,6 +151,55 @@ func WriteValidatorStatisticsForDay(day uint64) error {
 	}
 	logger.Infof("export completed, took %v", time.Since(start))
 
+	logger.Infof("exporting min_balance, max_balance, min_effective_balance, max_effective_balance, start_balance, start_effective_balance, end_balance and end_effective_balance statistics")
+	balanceStatistics, err := BigtableClient.GetValidatorBalanceStatistics(firstEpoch, lastEpoch)
+	if err != nil {
+		return err
+	}
+
+	balanceStatsArr := make([]*types.ValidatorBalanceStatistic, 0, len(balanceStatistics))
+	for _, stat := range balanceStatistics {
+		balanceStatsArr = append(balanceStatsArr, stat)
+	}
+
+	batchSize = 6500 // max parameters: 65535
+	for b := 0; b < len(balanceStatsArr); b += batchSize {
+		start := b
+		end := b + batchSize
+		if len(balanceStatsArr) < end {
+			end = len(balanceStatsArr)
+		}
+
+		numArgs := 10
+		valueStrings := make([]string, 0, batchSize)
+		valueArgs := make([]interface{}, 0, batchSize*numArgs)
+		for i, stat := range balanceStatsArr[start:end] {
+			valueStrings = append(valueStrings, fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d)", i*numArgs+1, i*numArgs+2, i*numArgs+3, i*numArgs+4, i*numArgs+5, i*numArgs+6, i*numArgs+7, i*numArgs+8, i*numArgs+9, i*numArgs+10))
+			valueArgs = append(valueArgs, stat.Index)
+			valueArgs = append(valueArgs, day)
+			valueArgs = append(valueArgs, stat.MinBalance)
+			valueArgs = append(valueArgs, stat.MaxBalance)
+			valueArgs = append(valueArgs, stat.MinEffectiveBalance)
+			valueArgs = append(valueArgs, stat.MaxEffectiveBalance)
+			valueArgs = append(valueArgs, stat.StartBalance)
+			valueArgs = append(valueArgs, stat.StartEffectiveBalance)
+			valueArgs = append(valueArgs, stat.EndBalance)
+			valueArgs = append(valueArgs, stat.EndEffectiveBalance)
+		}
+		stmt := fmt.Sprintf(`
+		insert into validator_stats (validatorindex, day, min_balance, max_balance, min_effective_balance, max_effective_balance, start_balance, start_effective_balance, end_balance, end_effective_balance) VALUES
+		%s
+		on conflict (validatorindex, day) do update set min_balance = excluded.min_balance, max_balance = excluded.max_balance, min_effective_balance = excluded.min_effective_balance, max_effective_balance = excluded.max_effective_balance, start_balance = excluded.start_balance, start_effective_balance = excluded.start_effective_balance, end_balance = excluded.end_balance, end_effective_balance = excluded.end_effective_balance;`,
+			strings.Join(valueStrings, ","))
+		_, err := tx.Exec(stmt, valueArgs...)
+		if err != nil {
+			return err
+		}
+
+		logger.Infof("saving validator balance batch %v completed", b)
+	}
+	logger.Infof("export completed, took %v", time.Since(start))
+
 	start = time.Now()
 	logger.Infof("exporting el_rewards_wei statistics")
 	incomeStats, err := BigtableClient.GetAggregatedValidatorIncomeDetailsHistory([]uint64{}, firstEpoch, lastEpoch)
@@ -337,55 +386,6 @@ func WriteValidatorStatisticsForDay(day uint64) error {
 	if err != nil {
 		return err
 	}
-
-	logger.Infof("exporting min_balance, max_balance, min_effective_balance, max_effective_balance, start_balance, start_effective_balance, end_balance and end_effective_balance statistics")
-	balanceStatistics, err := BigtableClient.GetValidatorBalanceStatistics(firstEpoch, lastEpoch)
-	if err != nil {
-		return err
-	}
-
-	balanceStatsArr := make([]*types.ValidatorBalanceStatistic, 0, len(balanceStatistics))
-	for _, stat := range balanceStatistics {
-		balanceStatsArr = append(balanceStatsArr, stat)
-	}
-
-	batchSize = 6500 // max parameters: 65535
-	for b := 0; b < len(balanceStatsArr); b += batchSize {
-		start := b
-		end := b + batchSize
-		if len(balanceStatsArr) < end {
-			end = len(balanceStatsArr)
-		}
-
-		numArgs := 10
-		valueStrings := make([]string, 0, batchSize)
-		valueArgs := make([]interface{}, 0, batchSize*numArgs)
-		for i, stat := range balanceStatsArr[start:end] {
-			valueStrings = append(valueStrings, fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d)", i*numArgs+1, i*numArgs+2, i*numArgs+3, i*numArgs+4, i*numArgs+5, i*numArgs+6, i*numArgs+7, i*numArgs+8, i*numArgs+9, i*numArgs+10))
-			valueArgs = append(valueArgs, stat.Index)
-			valueArgs = append(valueArgs, day)
-			valueArgs = append(valueArgs, stat.MinBalance)
-			valueArgs = append(valueArgs, stat.MaxBalance)
-			valueArgs = append(valueArgs, stat.MinEffectiveBalance)
-			valueArgs = append(valueArgs, stat.MaxEffectiveBalance)
-			valueArgs = append(valueArgs, stat.StartBalance)
-			valueArgs = append(valueArgs, stat.StartEffectiveBalance)
-			valueArgs = append(valueArgs, stat.EndBalance)
-			valueArgs = append(valueArgs, stat.EndEffectiveBalance)
-		}
-		stmt := fmt.Sprintf(`
-		insert into validator_stats (validatorindex, day, min_balance, max_balance, min_effective_balance, max_effective_balance, start_balance, start_effective_balance, end_balance, end_effective_balance) VALUES
-		%s
-		on conflict (validatorindex, day) do update set min_balance = excluded.min_balance, max_balance = excluded.max_balance, min_effective_balance = excluded.min_effective_balance, max_effective_balance = excluded.max_effective_balance, start_balance = excluded.start_balance, start_effective_balance = excluded.start_effective_balance, end_balance = excluded.end_balance, end_effective_balance = excluded.end_effective_balance;`,
-			strings.Join(valueStrings, ","))
-		_, err := tx.Exec(stmt, valueArgs...)
-		if err != nil {
-			return err
-		}
-
-		logger.Infof("saving validator balance batch %v completed", b)
-	}
-	logger.Infof("export completed, took %v", time.Since(start))
 
 	logger.Infof("populate validator_performance table")
 	_, err = tx.Exec(`insert into validator_performance (
