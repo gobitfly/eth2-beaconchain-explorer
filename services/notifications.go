bbd2a51238e6d52f88bd567270d939769ccac460
Patrick Pfeiffer <patrick@bitfly.at>
2023-05-31 17:10:29 +0200

    (BIDS-1846) notifications: more info on proposals

--- services/notifications.go
+++ services/notifications.go
@@ -1245,6 +1245,7 @@ func collectBlockProposalNotifications(notificationsByUserID map[uint64]map[type
 						SubscriptionID: *sub.ID,
 						ValidatorIndex: event.Proposer,
 						Epoch:          epoch,
+						Slot:           event.Slot,
 						Status:         event.Status,
 						EventName:      eventName,
 						Reward:         event.ExecRewardETH,
@@ -1274,6 +1275,7 @@ type validatorProposalNotification struct {
 	ValidatorIndex     uint64
 	ValidatorPublicKey string
 	Epoch              uint64
+	Slot               uint64
 	Status             uint64 // * Can be 0 = scheduled, 1 executed, 2 missed */
 	EventName          types.EventName
 	EventFilter        string
@@ -1312,11 +1314,11 @@ func (n *validatorProposalNotification) GetInfo(includeUrl bool) string {
 	var generalPart = ""
 	switch n.Status {
 	case 0:
-		generalPart = fmt.Sprintf(`New scheduled block proposal for Validator %v.`, n.ValidatorIndex)
+		generalPart = fmt.Sprintf(`New scheduled block proposal at slot <a href="https://%[1]v/slot/%[3]v">%[3]v</a> for Validator <a href="https://%[1]v/validator/%[2]v">%[2]v</a>.`, utils.Config.Frontend.SiteDomain, n.ValidatorIndex, n.Slot)
 	case 1:
-		generalPart = fmt.Sprintf(`Validator %v proposed a new block with %v ETH execution reward.`, n.ValidatorIndex, n.Reward)
+		generalPart = fmt.Sprintf(`Validator <a href="https://%[1]v/validator/%[2]v">%[2]v</a> proposed block at slot <a href="https://%[1]v/slot/%[3]v">%[3]v</a> with %[4]v %[5]v execution reward.`, utils.Config.Frontend.SiteDomain, n.ValidatorIndex, n.Slot, n.Reward, utils.Config.Frontend.ElCurrencySymbol)
 	case 2:
-		generalPart = fmt.Sprintf(`Validator %v missed a block proposal.`, n.ValidatorIndex)
+		generalPart = fmt.Sprintf(`Validator <a href="https://%[1]v/validator/%[2]v">%[2]v</a> missed a block proposal at slot <a href="https://%[1]v/slot/%[3]v">%[3]v</a>.`, utils.Config.Frontend.SiteDomain, n.ValidatorIndex, n.Slot)
 	}
 
 	if includeUrl {
@@ -1345,11 +1347,11 @@ func (n *validatorProposalNotification) GetInfoMarkdown() string {
 	var generalPart = ""
 	switch n.Status {
 	case 0:
-		generalPart = fmt.Sprintf(`New scheduled block proposal for Validator [%[1]v](https://%[2]v/%[1]v).`, n.ValidatorIndex, utils.Config.Frontend.SiteDomain+"/validator")
+		generalPart = fmt.Sprintf(`New scheduled block proposal at slot [%[3]v](https://%[1]v/slot/%[3]v) for Validator [%[2]v](https://%[1]v/validator/%[2]v).`, utils.Config.Frontend.SiteDomain, n.ValidatorIndex, n.Slot)
 	case 1:
-		generalPart = fmt.Sprintf(`Validator [%[1]v](https://%[2]v/%[1]v) proposed a new block with %[3]v ETH execution reward.`, n.ValidatorIndex, utils.Config.Frontend.SiteDomain+"/validator", n.Reward)
+		generalPart = fmt.Sprintf(`Validator [%[2]v](https://%[1]v/validator/%[2]v) proposed a new block at slot [%[3]v](https://%[1]v/slot/%[3]v) with %[4]v %[5]v execution reward.`, utils.Config.Frontend.SiteDomain, n.ValidatorIndex, n.Slot, n.Reward, utils.Config.Frontend.ElCurrencySymbol)
 	case 2:
-		generalPart = fmt.Sprintf(`Validator [%[1]v](https://%[2]v/%[1]v) missed a block proposal.`, n.ValidatorIndex, utils.Config.Frontend.SiteDomain+"/validator")
+		generalPart = fmt.Sprintf(`Validator [%[2]v](https://%[1]v/validator/%[2]v) missed a block proposal at slot [%[3]v](https://%[1]v/slot/%[3]v).`, utils.Config.Frontend.SiteDomain, n.ValidatorIndex, n.Slot)
 	}
 
 	return generalPart
