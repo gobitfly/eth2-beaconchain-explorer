7f4d0e87143d18176283b0524abba1b82e0bbd4c
Patrick Pfeiffer <patrick@bitfly.at>
2023-06-21 11:58:40 +0200

    (BIDS-2225) improve logging

--- db/frontend.go
+++ db/frontend.go
@@ -78,7 +78,7 @@ func GetUserIdByApiKey(apiKey string) (*types.UserWithPremium, error) {
 	go func() {
 		err = cache.TieredCache.Set(cacheKey, data, time.Minute*10)
 		if err != nil {
-			logger.WithField("cacheKey", cacheKey).WithError(err).Errorf("error setting tieredCache")
+			utils.LogError(err, fmt.Errorf("error setting tieredCache for GetUserIdByApiKey with key %v", cacheKey), 0)
 		}
 	}()
 	return data, err
