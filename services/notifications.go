ccf6b82aa04003054eaccab65016537910c5ab06
peter <1674920+peterbitfly@users.noreply.github.com>
2023-06-26 10:23:42 +0200

    (BIDS-2245) improve logging

--- services/notifications.go
+++ services/notifications.go
@@ -238,42 +238,42 @@ func collectNotifications(epoch uint64) (map[uint64]map[types.EventName][]types.
 		metrics.Errors.WithLabelValues("notifications_collect_missed_attestation").Inc()
 		return nil, fmt.Errorf("error collecting validator_attestation_missed notifications: %v", err)
 	}
-	logger.Infof("collecting attestation & offline notifications took: %v\n", time.Since(start))
+	logger.Infof("collecting attestation & offline notifications took: %v", time.Since(start))
 
 	err = collectBlockProposalNotifications(notificationsByUserID, 1, types.ValidatorExecutedProposalEventName, epoch)
 	if err != nil {
 		metrics.Errors.WithLabelValues("notifications_collect_executed_block_proposal").Inc()
 		return nil, fmt.Errorf("error collecting validator_proposal_submitted notifications: %v", err)
 	}
-	logger.Infof("collecting block proposal proposed notifications took: %v\n", time.Since(start))
+	logger.Infof("collecting block proposal proposed notifications took: %v", time.Since(start))
 
 	err = collectBlockProposalNotifications(notificationsByUserID, 2, types.ValidatorMissedProposalEventName, epoch)
 	if err != nil {
 		metrics.Errors.WithLabelValues("notifications_collect_missed_block_proposal").Inc()
 		return nil, fmt.Errorf("error collecting validator_proposal_missed notifications: %v", err)
 	}
-	logger.Infof("collecting block proposal missed notifications took: %v\n", time.Since(start))
+	logger.Infof("collecting block proposal missed notifications took: %v", time.Since(start))
 
 	err = collectValidatorGotSlashedNotifications(notificationsByUserID, epoch)
 	if err != nil {
 		metrics.Errors.WithLabelValues("notifications_collect_validator_got_slashed").Inc()
 		return nil, fmt.Errorf("error collecting validator_got_slashed notifications: %v", err)
 	}
-	logger.Infof("collecting validator got slashed notifications took: %v\n", time.Since(start))
+	logger.Infof("collecting validator got slashed notifications took: %v", time.Since(start))
 
 	err = collectWithdrawalNotifications(notificationsByUserID, epoch)
 	if err != nil {
 		metrics.Errors.WithLabelValues("notifications_collect_validator_withdrawal").Inc()
 		return nil, fmt.Errorf("error collecting withdrawal notifications: %v", err)
 	}
-	logger.Infof("collecting withdrawal notifications took: %v\n", time.Since(start))
+	logger.Infof("collecting withdrawal notifications took: %v", time.Since(start))
 
 	err = collectNetworkNotifications(notificationsByUserID, types.NetworkLivenessIncreasedEventName)
 	if err != nil {
 		metrics.Errors.WithLabelValues("notifications_collect_network").Inc()
 		return nil, fmt.Errorf("error collecting network notifications: %v", err)
 	}
-	logger.Infof("collecting network notifications took: %v\n", time.Since(start))
+	logger.Infof("collecting network notifications took: %v", time.Since(start))
 
 	// Rocketpool
 	{
@@ -293,28 +293,28 @@ func collectNotifications(epoch uint64) (map[uint64]map[types.EventName][]types.
 				metrics.Errors.WithLabelValues("notifications_collect_rocketpool_comission").Inc()
 				return nil, fmt.Errorf("error collecting rocketpool commission: %v", err)
 			}
-			logger.Infof("collecting rocketpool commissions took: %v\n", time.Since(start))
+			logger.Infof("collecting rocketpool commissions took: %v", time.Since(start))
 
 			err = collectRocketpoolRewardClaimRoundNotifications(notificationsByUserID, types.RocketpoolNewClaimRoundStartedEventName)
 			if err != nil {
 				metrics.Errors.WithLabelValues("notifications_collect_rocketpool_reward_claim").Inc()
 				return nil, fmt.Errorf("error collecting new rocketpool claim round: %v", err)
 			}
-			logger.Infof("collecting rocketpool claim round took: %v\n", time.Since(start))
+			logger.Infof("collecting rocketpool claim round took: %v", time.Since(start))
 
 			err = collectRocketpoolRPLCollateralNotifications(notificationsByUserID, types.RocketpoolCollateralMaxReached, epoch)
 			if err != nil {
 				metrics.Errors.WithLabelValues("notifications_collect_rocketpool_rpl_collateral_max_reached").Inc()
 				return nil, fmt.Errorf("error collecting rocketpool max collateral: %v", err)
 			}
-			logger.Infof("collecting rocketpool max collateral took: %v\n", time.Since(start))
+			logger.Infof("collecting rocketpool max collateral took: %v", time.Since(start))
 
 			err = collectRocketpoolRPLCollateralNotifications(notificationsByUserID, types.RocketpoolCollateralMinReached, epoch)
 			if err != nil {
 				metrics.Errors.WithLabelValues("notifications_collect_rocketpool_rpl_collateral_min_reached").Inc()
 				return nil, fmt.Errorf("error collecting rocketpool min collateral: %v", err)
 			}
-			logger.Infof("collecting rocketpool min collateral took: %v\n", time.Since(start))
+			logger.Infof("collecting rocketpool min collateral took: %v", time.Since(start))
 		}
 	}
 
@@ -323,7 +323,7 @@ func collectNotifications(epoch uint64) (map[uint64]map[types.EventName][]types.
 		metrics.Errors.WithLabelValues("notifications_collect_sync_committee").Inc()
 		return nil, fmt.Errorf("error collecting sync committee: %v", err)
 	}
-	logger.Infof("collecting sync committee took: %v\n", time.Since(start))
+	logger.Infof("collecting sync committee took: %v", time.Since(start))
 
 	return notificationsByUserID, nil
 }
@@ -855,7 +855,7 @@ func queueWebhookNotifications(notificationsByUserID map[uint64]map[types.EventN
 								continue
 							}
 						} else if w.Retries > 5 && !w.LastSent.Valid {
-							logger.Warn("error webhook has more than 5 retries and does not have a valid last_sent timestamp")
+							logger.Warnf("error webhook '%v' has more than 5 retries and does not have a valid last_sent timestamp", w.Url)
 							continue
 						}
 
