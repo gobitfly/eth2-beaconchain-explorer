34a3803b096c522dd77ca0c23023b636f4fba7c8
LUCCA DUKIC <109136188+LuccaBitfly@users.noreply.github.com>
2023-07-03 10:06:33 +0200

    (BIDS-2252) fix indexed_validators search

--- handlers/search.go
+++ handlers/search.go
@@ -225,7 +225,7 @@ func SearchAhead(w http.ResponseWriter, r *http.Request) {
 	case "indexed_validators":
 		// find all validators that have a publickey or index like the search-query
 		result = &types.SearchAheadValidatorsResult{}
-		indexNumeric, errParse := strconv.ParseInt(search, 10, 64)
+		indexNumeric, errParse := strconv.ParseInt(search, 10, 32)
 		if errParse == nil { // search the validator by its index
 			err = db.ReaderDb.Select(result, `SELECT validatorindex AS index, pubkeyhex as pubkey FROM validators WHERE validatorindex = $1`, indexNumeric)
 			if err != nil {
