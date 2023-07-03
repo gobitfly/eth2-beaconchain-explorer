c36eef4fe27a5ab9ce0372db16f9a139122c1bc5
Patrick Pfeiffer <patrick@bitfly.at>
2023-07-03 14:10:06 +0200

    (BIDS-2258) split db migrations into indices and tables

--- db/migrations/20230629144437_add_graffiti_stats_tables.sql
+++ db/migrations/20230629144437_add_graffiti_stats_tables.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query - remove graffiti_stats tables';
+SELECT 'up SQL query - add graffiti_stats tables';
 CREATE TABLE IF NOT EXISTS
     graffiti_stats (
         day INTEGER NOT NULL,
@@ -10,14 +10,10 @@ CREATE TABLE IF NOT EXISTS
         proposer_count INTEGER NOT NULL,
         PRIMARY KEY (graffiti, day)
     );
-CREATE INDEX IF NOT EXISTS idx_graffiti_stats_day ON graffiti_stats (day);
-CREATE INDEX IF NOT EXISTS idx_graffiti_stats_day_graffiti_text ON graffiti_stats USING gin (graffiti_text gin_trgm_ops);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query - remove ens lookup tables';
-DROP INDEX IF EXISTS idx_graffiti_stats_day;
-DROP INDEX IF EXISTS idx_graffiti_stats_day_graffiti_text;
+SELECT 'down SQL query - remove graffiti_stats tables';
 DROP TABLE IF EXISTS graffiti_stats;
 -- +goose StatementEnd
