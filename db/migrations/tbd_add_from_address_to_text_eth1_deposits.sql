e29c5e1061f84896760fe24fd0b76e4044cb03ba
Alexander Maushammer <alexander.maushammer@bitfly.at>
2023-08-11 10:55:54 +0200

    (BIDS-2249) add from_address_text column

---
+++ db/migrations/tbd_add_from_address_to_text_eth1_deposits.sql
@@ -0,0 +1,42 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query - add column eth1_deposits for from_address_text';
+ALTER TABLE eth1_deposits ADD COLUMN from_address_text TEXT NOT NULL DEFAULT '';
+
+SELECT 'create new eth1_deposits index for from_address_text'; 
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_eth1_deposits_from_address_text ON eth1_deposits (from_address_text);
+
+SELECT 'populate new eth1_deposits column from_address_text, 1000 at a time'; 
+DO
+$do$
+DECLARE
+    count INTEGER default 0;
+BEGIN
+LOOP
+    count := (count + 1);
+    RAISE NOTICE 'count = %', count;
+    EXIT WHEN (SELECT count(*) from (SELECT from_address_text FROM eth1_deposits WHERE from_address_text = '' LIMIT(1)) as sub) = 0;
+    WITH to_update AS (
+        SELECT tx_hash, merkletree_index
+        FROM eth1_deposits
+        WHERE from_address_text = ''
+        LIMIT 1000
+    )
+    UPDATE eth1_deposits SET from_address_text = ENCODE(from_address, 'hex')  
+    WHERE EXISTS (SELECT * FROM to_update 
+   	    WHERE eth1_deposits.tx_hash = to_update.tx_hash
+	    AND eth1_deposits.merkletree_index = to_update.merkletree_index);
+END LOOP;
+END;
+$do$;
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query - drop from_address_text index from eth1_deposits '; 
+DROP INDEX IF EXISTS idx_eth1_deposits_from_address_text;
+
+SELECT 'drop column from_address_text from eth1_deposits';
+ALTER TABLE eth1_deposits DROP COLUMN from_address_text;
+-- +goose StatementEnd
