e07c3b32eafd4b30913eb0543f04e9f0641f96ce
RamiRond <ramirond@beaconcha.in>
2023-08-18 14:46:03 +0200

    (BIDS-2183) fixed infinite loop

--- static/js/layout.js
+++ static/js/layout.js
@@ -181,7 +181,6 @@ function activateTabbarSwitcher(tabContainerId, tabBar, defaultTab) {
     }
 
     new bootstrap.Tab(someTabTriggerEl[0]).show()
-    $(`[href='#${selectedTab}']`).trigger("shown")
   }
   const handleTabChangeClick = (event) => {
     var href = event.currentTarget.href
