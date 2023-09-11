5ca091188bdb58bb86886ab8b941f6ee378b313e
peter <1674920+peterbitfly@users.noreply.github.com>
2023-09-11 09:28:53 +0200

    (BIDS-2379) fix build errors

--- cmd/migrations/bigtable/main.go
+++ cmd/migrations/bigtable/main.go
@@ -9,7 +9,6 @@ import (
 	"flag"
 	"fmt"
 	"math/big"
-	"strconv"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -66,31 +65,6 @@ func main() {
 			utils.LogFatal(err, "deleting epoch error", 0)
 		}
 
-		firstSlot := i * utils.Config.Chain.Config.SlotsPerEpoch
-		lastSlot := (i+1)*utils.Config.Chain.Config.SlotsPerEpoch - 1
-
-		c, err := rpcClient.GetSyncCommittee(fmt.Sprintf("%d", firstSlot), i)
-		if err != nil {
-			utils.LogFatal(err, "getting sync comittee error", 0)
-		}
-
-		validatorsU64 := make([]uint64, len(c.Validators))
-		for i, idxStr := range c.Validators {
-			idxU64, err := strconv.ParseUint(idxStr, 10, 64)
-			if err != nil {
-				utils.LogFatal(err, "parsing validator index to uint error", 0)
-			}
-			validatorsU64[i] = idxU64
-		}
-
-		logrus.Infof("saving sync assignments for %v validators", len(validatorsU64))
-
-		err = db.BigtableClient.SaveSyncCommitteesAssignments(firstSlot, lastSlot, validatorsU64)
-		if err != nil {
-			logrus.Fatalf("error saving sync committee assignments: %v", err)
-		}
-		logrus.Infof("exported sync committee assignments to bigtable in %v", i)
-
 		data, err := rpcClient.GetEpochData(uint64(i), true)
 		if err != nil {
 			utils.LogFatal(err, "getting epoch data error", 0)
@@ -99,27 +73,32 @@ func main() {
 		g := new(errgroup.Group)
 
 		g.Go(func() error {
-			return bt.SaveValidatorBalances(data.Epoch, data.Validators)
+			err = db.BigtableClient.SaveProposalAssignments(i, data.ValidatorAssignmentes.ProposerAssignments)
+			if err != nil {
+				return fmt.Errorf("error exporting proposal assignments to bigtable: %v", err)
+			}
+			return nil
 		})
-
 		g.Go(func() error {
-			return bt.SaveAttestationAssignments(data.Epoch, data.ValidatorAssignmentes.AttestorAssignments)
+			err = db.BigtableClient.SaveAttestationDuties(data.AttestationDuties)
+			if err != nil {
+				return fmt.Errorf("error exporting attestations to bigtable: %v", err)
+			}
+			return nil
 		})
-
 		g.Go(func() error {
-			return bt.SaveProposalAssignments(data.Epoch, data.ValidatorAssignmentes.ProposerAssignments)
+			err = db.BigtableClient.SaveProposals(data.Blocks)
+			if err != nil {
+				return fmt.Errorf("error exporting proposals to bigtable: %v", err)
+			}
+			return nil
 		})
-
 		g.Go(func() error {
-			return bt.SaveAttestations(data.Blocks)
-		})
-
-		g.Go(func() error {
-			return bt.SaveProposals(data.Blocks)
-		})
-
-		g.Go(func() error {
-			return bt.SaveSyncComitteeDuties(data.Blocks)
+			err = db.BigtableClient.SaveSyncComitteeDuties(data.SyncDuties)
+			if err != nil {
+				return fmt.Errorf("error exporting sync committee duties to bigtable: %v", err)
+			}
+			return nil
 		})
 
 		err = g.Wait()
@@ -177,27 +156,32 @@ func monitor(configPath string) {
 			g := new(errgroup.Group)
 
 			g.Go(func() error {
-				return bt.SaveValidatorBalances(data.Epoch, data.Validators)
+				err = db.BigtableClient.SaveProposalAssignments(i, data.ValidatorAssignmentes.ProposerAssignments)
+				if err != nil {
+					return fmt.Errorf("error exporting proposal assignments to bigtable: %v", err)
+				}
+				return nil
 			})
-
 			g.Go(func() error {
-				return bt.SaveAttestationAssignments(data.Epoch, data.ValidatorAssignmentes.AttestorAssignments)
+				err = db.BigtableClient.SaveAttestationDuties(data.AttestationDuties)
+				if err != nil {
+					return fmt.Errorf("error exporting attestations to bigtable: %v", err)
+				}
+				return nil
 			})
-
 			g.Go(func() error {
-				return bt.SaveProposalAssignments(data.Epoch, data.ValidatorAssignmentes.ProposerAssignments)
+				err = db.BigtableClient.SaveProposals(data.Blocks)
+				if err != nil {
+					return fmt.Errorf("error exporting proposals to bigtable: %v", err)
+				}
+				return nil
 			})
-
 			g.Go(func() error {
-				return bt.SaveAttestations(data.Blocks)
-			})
-
-			g.Go(func() error {
-				return bt.SaveProposals(data.Blocks)
-			})
-
-			g.Go(func() error {
-				return bt.SaveSyncComitteeDuties(data.Blocks)
+				err = db.BigtableClient.SaveSyncComitteeDuties(data.SyncDuties)
+				if err != nil {
+					return fmt.Errorf("error exporting sync committee duties to bigtable: %v", err)
+				}
+				return nil
 			})
 
 			err = g.Wait()
