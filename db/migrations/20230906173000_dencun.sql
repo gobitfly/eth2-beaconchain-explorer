afc64aadcdc111eb45cd263ecf4c6e01afd4964f
Patrick Pfeiffer <patrick@bitfly.at>
2023-10-03 13:17:46 +0200

    (BIDS-2346) db: update dencun-migrations (add down stmts

--- db/migrations/20230906173000_dencun.sql
+++ db/migrations/20230906173000_dencun.sql
@@ -21,4 +21,34 @@ CREATE TABLE IF NOT EXISTS
 
 -- +goose Down
 -- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS
+    blocks_transactions (
+        block_slot INT NOT NULL,
+        block_index INT NOT NULL,
+        block_root bytea NOT NULL DEFAULT '',
+        raw bytea NOT NULL,
+        txhash bytea NOT NULL,
+        nonce INT NOT NULL,
+        gas_price bytea NOT NULL,
+        gas_limit BIGINT NOT NULL,
+        sender bytea NOT NULL,
+        recipient bytea NOT NULL,
+        amount bytea NOT NULL,
+        payload bytea NOT NULL,
+        max_priority_fee_per_gas BIGINT,
+        max_fee_per_gas BIGINT,
+        PRIMARY KEY (block_slot, block_index)
+    );
+
+CREATE TABLE IF NOT EXISTS
+    validator_balances_recent (
+        epoch INT NOT NULL,
+        validatorindex INT NOT NULL,
+        balance BIGINT NOT NULL,
+        PRIMARY KEY (epoch, validatorindex)
+    );
+
+ALTER TABLE blocks DROP COLUMN IF EXISTS exec_blob_gas_used;
+ALTER TABLE blocks DROP COLUMN IF EXISTS exec_excess_blob_gas;
+ALTER TABLE blocks DROP COLUMN IF EXISTS exec_blob_transactions_count;
 -- +goose StatementEnd
