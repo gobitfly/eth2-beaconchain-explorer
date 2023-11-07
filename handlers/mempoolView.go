90dd7c924df84c61b2114099f7b0fb6996feb1fe
LUCCA DUKIC <109136188+LuccaBitfly@users.noreply.github.com>
2023-11-07 13:39:13 +0100

    (BIDS-2619) fix vali history tooltip

--- handlers/mempoolView.go
+++ handlers/mempoolView.go
@@ -65,7 +65,7 @@ func toTableDataRow(tx *types.RawMempoolTransaction) []interface{} {
 		utils.FormatAddressAll(tx.From.Bytes(), "", false, "address", "", int(12), int(12), true),
 		_isContractCreation(tx.To),
 		utils.FormatAmount((*big.Int)(tx.Value), utils.Config.Frontend.ElCurrency, 5),
-		utils.FormatAddCommasFormated(float64(tx.Gas.ToInt().Int64()), 0),
+		utils.FormatAddCommasFormatted(float64(tx.Gas.ToInt().Int64()), 0),
 		utils.FormatAmountFormatted(tx.GasPrice.ToInt(), "GWei", 5, 0, true, true, false),
 		tx.Nonce.ToInt(),
 	}
