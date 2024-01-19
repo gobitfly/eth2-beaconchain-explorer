6030fd4822a4450c930c75163debad61a7baac9d
spletka <stefan.pletka@bitfly.at>
2024-01-19 11:44:45 +0100

    (BIDS-2624) Add the datatable query to the URL

--- handlers/epochs.go
+++ handlers/epochs.go
@@ -35,6 +35,12 @@ func EpochsData(w http.ResponseWriter, r *http.Request) {
 
 	q := r.URL.Query()
 
+	emptySearch := false
+
+	if len(q.Get("search[value]")) == 0 {
+		emptySearch = true
+	}
+
 	search, err := strconv.ParseInt(q.Get("search[value]"), 10, 64)
 	if err != nil {
 		search = -1
@@ -77,7 +83,7 @@ func EpochsData(w http.ResponseWriter, r *http.Request) {
 	var epochs []*types.EpochsPageData
 
 	latestFinalizedEpoch := services.LatestFinalizedEpoch()
-	if search == -1 {
+	if emptySearch {
 		err = db.ReaderDb.Select(&epochs, `
 			SELECT epoch, 
 				blockscount, 
@@ -96,7 +102,7 @@ func EpochsData(w http.ResponseWriter, r *http.Request) {
 			FROM epochs 
 			WHERE epoch >= $1 AND epoch <= $2
 			ORDER BY epoch DESC`, endEpoch, startEpoch, latestFinalizedEpoch)
-	} else {
+	} else if search != -1 {
 		err = db.ReaderDb.Select(&epochs, `
 			SELECT epoch, 
 				blockscount, 
