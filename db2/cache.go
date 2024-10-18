583f3069e92424e6a0a62f96dbea52183930adc3
Monika-Bitfly <181824500+Monika-Bitfly@users.noreply.github.com>
2024-10-18 16:20:40 +0000

    wip: updated cache

--- db2/cache.go
+++ db2/cache.go
@@ -15,6 +15,11 @@ type MinimalBlock struct {
 	} `json:"result"`
 }
 
+type CachedBlock struct {
+	Data *FullBlockRawData
+	Hash string
+}
+
 type CachedRawStore struct {
 	db RawStoreReader
 	// sync.Map with manual delete have better perf than freecache because we can handle this way a ttl < 1s
@@ -29,40 +34,52 @@ func WithCache(reader RawStoreReader) *CachedRawStore {
 
 func (c *CachedRawStore) ReadBlockByNumber(chainID uint64, number int64) (*FullBlockRawData, error) {
 	key := blockKey(chainID, number)
+
 	v, ok := c.cache.Load(key)
 	if ok {
-		return v.(*FullBlockRawData), nil
+		return v.(*CachedBlock).Data, nil
 	}
 
 	block, err := c.db.ReadBlockByNumber(chainID, number)
-	if block != nil {
-		c.cache.Store(key, block)
-
-		// retrieve the block hash for caching purpose
-		var mini MinimalBlock
-		if err := json.Unmarshal(block.Block, &mini); err != nil {
-			return nil, fmt.Errorf("cannot unmarshal block: %w", err)
-		}
-		c.cache.Store(mini.Result.Hash, number)
-		go func() {
-			time.Sleep(ttl)
-			c.cache.Delete(key)
-			c.cache.Delete(mini.Result.Hash)
-		}()
+	if err != nil || block == nil {
+		return block, err
 	}
-	return block, err
+
+	cachedBlock := &CachedBlock{Data: block}
+
+	// retrieve the block hash for caching purpose
+	var mini MinimalBlock
+	if err := json.Unmarshal(block.Block, &mini); err != nil {
+		return nil, fmt.Errorf("cannot unmarshal block: %w", err)
+	}
+
+	cachedBlock.Hash = mini.Result.Hash
+	c.cache.Store(key, cachedBlock)
+
+	go func() {
+		time.Sleep(ttl)
+		c.cache.Delete(key)
+		c.cache.Delete(mini.Result.Hash)
+	}()
+
+	return block, nil
 }
 
 func (c *CachedRawStore) ReadBlockByHash(chainID uint64, hash string) (*FullBlockRawData, error) {
 	v, ok := c.cache.Load(hash)
 	if !ok {
-		return c.db.ReadBlockByHash(chainID, hash)
+		return v.(*CachedBlock).Data, nil
 	}
 
-	v, ok = c.cache.Load(blockKey(chainID, v.(int64)))
-	if !ok {
-		return c.db.ReadBlockByHash(chainID, hash)
+	blockData, err := c.db.ReadBlockByHash(chainID, hash)
+	if err != nil || blockData == nil {
+		return blockData, err
 	}
 
-	return v.(*FullBlockRawData), nil
+	key := blockKey(chainID, blockData.BlockNumber)
+	if _, ok := c.cache.Load(key); ok {
+		return blockData, nil
+	}
+
+	return c.db.ReadBlockByHash(chainID, hash)
 }
