eb64e6edd6970b1fc21dc2a0d6ea12f594f24baf
GlacierWalrus <114941647+GlacierWalrus@users.noreply.github.com>
2025-01-15 22:10:57 +0000

    Add notransaction fix
    Disable transaction around `CREATE INDEX CONCURRENTLY` statements

--- db/migrations/20250112125552_add_idx_blocks_status_depositscountgt0.sql
+++ db/migrations/20250112125552_add_idx_blocks_status_depositscountgt0.sql
@@ -1,13 +1,13 @@
 -- +goose NO TRANSACTION
 
 -- +goose Up
--- +goose StatementBegin
 SELECT 'creating idx_blocks_status_depositscountgt0';
+-- +goose StatementBegin
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_blocks_status_depositscountgt0 ON blocks (status, (depositscount > 0)) where depositscount > 0;
 -- +goose StatementEnd
 
 -- +goose Down
--- +goose StatementBegin
 SELECT 'dropping idx_blocks_status_depositscountgt0';
+-- +goose StatementBegin
 DROP INDEX CONCURRENTLY IF EXISTS idx_blocks_status_depositscountgt0;
 -- +goose StatementEnd
